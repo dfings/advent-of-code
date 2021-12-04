694aedbbcfeec54f2ef3c6e254b46eafd8cd0651
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-04 01:23:52 -0800

    Problem 1 second star

--- src/2021/problem_1.main.kts
+++ src/2021/problem_1.main.kts
@@ -2,5 +2,7 @@
 
 import java.io.File
 
+fun Boolean.toInt() = if (this) 1 else 0
 val data = File(args[0]).readLines().map { it.toInt() }
-println(data.windowed(2) { if (it[0] < it[1]) 1 else 0}.sum())
+println(data.windowed(2) { (it[0] < it[1]).toInt() }.sum())
+println(data.windowed(4) { (it.dropLast(1).sum() < it.drop(1).sum()).toInt() }.sum())
