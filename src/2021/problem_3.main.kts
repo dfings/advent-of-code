f7140f6df96786e13bcd84005f64e0d92acb54cc
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-04 03:58:51 -0800

    Simplify problem 3

--- src/2021/problem_3.main.kts
+++ src/2021/problem_3.main.kts
@@ -4,17 +4,19 @@ import java.io.File
 
 val data = File(args[0]).readLines().map { it.toList().map(Character::getNumericValue) }
 
+fun List<Int>.toBinaryInt() = this.joinToString("").toInt(2)
+
 // Part 1
 val bitCount = data.reduce { acc, it -> acc.zip(it, Int::plus) }
 val cutoff = data.size / 2
-val gamma = bitCount.map { if (it > cutoff) 1 else 0 }.joinToString("")
-val epsilon = bitCount.map { if (it < cutoff) 1 else 0  }.joinToString("")
-println(gamma.toInt(2) * epsilon.toInt(2))
+val gamma = bitCount.map { if (it > cutoff) 1 else 0 }.toBinaryInt()
+val epsilon = bitCount.map { if (it < cutoff) 1 else 0  }.toBinaryInt()
+println(gamma * epsilon)
 
 // Part 2
-fun getRating(subset: List<List<Int>>, mostCommon: Boolean, position: Int = 0): List<Int> {
+fun getRating(subset: List<List<Int>>, mostCommon: Boolean, position: Int = 0): Int {
     return if (subset.size == 1) {
-        subset[0]
+        subset[0].toBinaryInt()
     } else {
         val setCount = subset.count { it[position] == 1 }
         val selected = when {
@@ -25,6 +27,6 @@ fun getRating(subset: List<List<Int>>, mostCommon: Boolean, position: Int = 0): 
         getRating(subset.filter { it[position] == selected }, mostCommon, position + 1)
     }
 }
-val oxygen = getRating(data, mostCommon = true).joinToString("")
-val co2 = getRating(data, mostCommon = false).joinToString("")
-println(oxygen.toInt(2) * co2.toInt(2))
+val oxygen = getRating(data, mostCommon = true)
+val co2 = getRating(data, mostCommon = false)
+println(oxygen * co2)
