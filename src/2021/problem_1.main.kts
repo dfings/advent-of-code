82ce6b56961293366c49f9a06e924f8679601eb2
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-04 01:52:20 -0800

    Simplify problem 1

--- src/2021/problem_1.main.kts
+++ src/2021/problem_1.main.kts
@@ -2,7 +2,6 @@
 
 import java.io.File
 
-fun Boolean.toInt() = if (this) 1 else 0
 val data = File(args[0]).readLines().map { it.toInt() }
-println(data.windowed(2) { (it[0] < it[1]).toInt() }.sum())
-println(data.windowed(4) { (it.dropLast(1).sum() < it.drop(1).sum()).toInt() }.sum())
+println(data.windowed(2).count { it[0] < it[1] })
+println(data.windowed(4).count { it.dropLast(1).sum() < it.drop(1).sum() })
