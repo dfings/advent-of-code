df83d10794d38429033ca99c64dc019a75512640
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-04 01:49:07 -0800

    Problem 2 solution

---
+++ src/2021/problem_2.main.kts
@@ -0,0 +1,37 @@
+#!/usr/bin/env kotlin
+
+import java.io.File
+
+data class Command(val direction: String, val step: Int)
+val cmds = File(args[0])
+    .readLines()
+    .map { it.split(" ") }
+    .map { Command(it[0], it[1].toInt())}
+
+// Part 1
+var position = 0
+var depth = 0
+cmds.forEach {
+    when (it.direction) {
+        "forward" -> position += it.step
+        "up" -> depth -= it.step
+        "down" -> depth += it.step
+    }
+}
+println(position * depth)
+
+// Part 2
+position = 0
+depth = 0
+var aim = 0
+cmds.forEach {
+    when (it.direction) {
+        "forward" -> {
+            position += it.step
+            depth += aim * it.step
+        }
+        "up" -> aim -= it.step
+        "down" -> aim += it.step
+    }
+}
+println(position * depth)
