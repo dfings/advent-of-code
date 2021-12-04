dd7c7dc7d7a7f6d08be1bbff66cca251e55e2a55
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-04 03:52:28 -0800

    Problem 3 part 2

--- src/2021/problem_3.main.kts
+++ src/2021/problem_3.main.kts
@@ -10,3 +10,21 @@ val cutoff = data.size / 2
 val gamma = bitCount.map { if (it > cutoff) 1 else 0 }.joinToString("")
 val epsilon = bitCount.map { if (it < cutoff) 1 else 0  }.joinToString("")
 println(gamma.toInt(2) * epsilon.toInt(2))
+
+// Part 2
+fun getRating(subset: List<List<Int>>, mostCommon: Boolean, position: Int = 0): List<Int> {
+    return if (subset.size == 1) {
+        subset[0]
+    } else {
+        val setCount = subset.count { it[position] == 1 }
+        val selected = when {
+            setCount >= subset.size / 2 && mostCommon -> 1
+            setCount < subset.size / 2 && !mostCommon -> 1
+            else -> 0
+        }
+        getRating(subset.filter { it[position] == selected }, mostCommon, position + 1)
+    }
+}
+val oxygen = getRating(data, mostCommon = true).joinToString("")
+val co2 = getRating(data, mostCommon = false).joinToString("")
+println(oxygen.toInt(2) * co2.toInt(2))
