275dc11ca252b0d00d0834ce68995427b8536dd3
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-05 15:43:09 -0800

    Problem 5 part 2

--- src/2021/problem_5.main.kts
+++ src/2021/problem_5.main.kts
@@ -2,10 +2,12 @@
 
 data class Point(val x: Int, val y: Int)
 data class Line(val a: Point, val b: Point) {
-    fun contains(p: Point) = when {
+    val diagonal: Boolean = a.x != b.x && a.y != b.y
+    fun contains(p: Point): Boolean = when {
         a.x == b.x -> p.x == a.x && a.y <= p.y && p.y <= b.y
         a.y == b.y -> p.y == a.y && a.x <= p.x && p.x <= b.x
-        else -> false
+        a.y < b.y -> a.x <= p.x && a.y <= p.y && p.x <= b.x && p.y <= b.y && p.x - a.x == p.y - a.y
+        else -> a.x <= p.x && b.y <= p.y && p.x <= b.x && p.y <= a.y && p.x - a.x == a.y - p.y
     }
 }
 
@@ -13,12 +15,16 @@ fun parsePoint(raw: List<String>) = Point(raw[0].toInt(), raw[1].toInt())
 fun parseLine(raw: List<String>): Line {
     val a = parsePoint(raw.take(2))
     val b = parsePoint(raw.drop(2))
-    return if (a.x <= b.x  && a.y <= b.y) Line(a, b) else Line(b, a)
+    return if (a.x < b.x  || (a.x == b.x && a.y < b.y)) Line(a, b) else Line(b, a)
 }
 val lines = java.io.File(args[0]).readLines().map { parseLine(it.split(",", " -> ")) }
+val nonDiagonal = lines.filterNot { it.diagonal }
 
 val maxX = lines.maxOf { it.b.x }
 val maxY = lines.maxOf { it.b.y }
 val points = (0..maxX).flatMap { x -> (0..maxY).map { y -> Point(x, y) } }
-val overlap = points.filter { p -> lines.count { it.contains(p) } > 1 }
-println(overlap.size)
+val overlap1 = points.filter { p -> nonDiagonal.count { it.contains(p) } > 1 }
+val overlap2 = points.filter { p -> lines.count { it.contains(p) } > 1 }
+
+println(overlap1.size)
+println(overlap2.size)
