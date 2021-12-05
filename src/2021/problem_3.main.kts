bb19ceed3fd7f107a2f88ca8a2e2ec3ef1efb97d
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-05 00:55:19 -0800

    Update problem_3.main.kts

--- src/2021/problem_3.main.kts
+++ src/2021/problem_3.main.kts
@@ -2,31 +2,29 @@
 
 import java.io.File
 
-val data = File(args[0]).readLines().map { it.toList().map(Character::getNumericValue) }
+val lines = File(args[0]).readLines().map { it.toList().map(Character::getNumericValue) }
 
 fun List<Int>.toBinaryInt() = this.joinToString("").toInt(2)
 
 // Part 1
-val bitCount = data.reduce { acc, it -> acc.zip(it, Int::plus) }
-val cutoff = data.size / 2
-val gamma = bitCount.map { if (it >= cutoff) 1 else 0 }.toBinaryInt()
-val epsilon = bitCount.map { if (it < cutoff) 1 else 0  }.toBinaryInt()
+// Count of 1 bits at each position (complement is unsetCount).
+val setCount: List<Int> = lines.reduce { acc, it -> acc.zip(it, Int::plus) }
+val cutoff = lines.size / 2
+val gamma = setCount.map { if (it >= cutoff) 1 else 0 }.toBinaryInt()
+val epsilon = setCount.map { if (it < cutoff) 1 else 0  }.toBinaryInt()
 println(gamma * epsilon)
 
 // Part 2
-fun getRating(subset: List<List<Int>>, mostCommon: Boolean, position: Int = 0): Int {
+fun getRating(subset: List<List<Int>>, position: Int = 0, bitSelector: (Boolean) -> Int): Int {
     return if (subset.size == 1) {
         subset[0].toBinaryInt()
     } else {
-        val setCount = subset.count { it[position] == 1 }
-        val selected = when {
-            setCount >= subset.size / 2 && mostCommon -> 1
-            setCount < subset.size / 2 && !mostCommon -> 1
-            else -> 0
-        }
-        getRating(subset.filter { it[position] == selected }, mostCommon, position + 1)
+        val positionSetCount = subset.count { it[position] == 1 }
+        val positionFilter = bitSelector(positionSetCount >= subset.size / 2)
+        getRating(subset.filter { it[position] == positionFilter }, position + 1, bitSelector)
     }
 }
-val oxygen = getRating(data, mostCommon = true)
-val co2 = getRating(data, mostCommon = false)
+
+val oxygen = getRating(lines) { if (it) 1 else 0}
+val co2 = getRating(lines) { if (it) 0 else 1 }
 println(oxygen * co2)
