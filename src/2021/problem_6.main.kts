b60cc08e0d4526e187442fa1160da07ed9aa7851
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-06 00:42:16 -0800

    Reimplement as a state machine using a deque

--- src/2021/problem_6.main.kts
+++ src/2021/problem_6.main.kts
@@ -1,17 +1,17 @@
 #!/usr/bin/env kotlin
 
-fun runGenerations(start: Map<Int, Long>, generations: Int): Map<Int, Long> =
-    generateSequence(0 to start) { (i, fish) ->
-        if (i == generations) {
-            null
-        } else {
-            i + 1 to fish.flatMap { (timer, count) ->
-                if (timer == 0) listOf(8 to count, 6 to count) else listOf(timer - 1 to count)
-            }.groupBy { it.first }.mapValues { it.value.sumOf { it.second } }
-        }
-    }.last().second
+fun runGenerations(start: List<Long>, generations: Int): List<Long> {
+    var state = ArrayDeque(start)
+    repeat (generations) { 
+        val spawn = state.removeFirst()
+        state.add(spawn)
+        state[6] += spawn
+    }
+    return state.toList()
+}
     
-val input = java.io.File(args[0]).readLines().first().split(",")
-val start = input.groupingBy { it.toInt() }.eachCount().mapValues { it.value.toLong() }
-println(runGenerations(start, 80).values.sum())
-println(runGenerations(start, 256).values.sum())
+val start = MutableList(9) { 0L }
+java.io.File(args[0]).readLines().first().split(",").forEach { start[it.toInt()]++ }
+
+println(runGenerations(start, 80).sum())
+println(runGenerations(start, 256).sum())
