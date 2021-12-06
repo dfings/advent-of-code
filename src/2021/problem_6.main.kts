742749a2b90f2cef6b553d603561e7817165848c
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-05 23:41:51 -0800

    Problem 6 part 2 first cut

--- src/2021/problem_6.main.kts
+++ src/2021/problem_6.main.kts
@@ -1,8 +1,24 @@
 #!/usr/bin/env kotlin
 
-val spawn = listOf(6, 8)
-val fishStart = java.io.File(args[0]).readLines().first().split(",").map { it.toInt() }
-val fishEnd = generateSequence(0 to fishStart) { (i, fish) ->
-    if (i == 80) null else i + 1 to fish.flatMap { if (it > 0) listOf(it - 1) else spawn }
-}.last().second
-println(fishEnd.size)
+fun runGenerations(start: Map<Int, Long>, generations: Int): Map<Int, Long> =
+    generateSequence(0 to start) { (i, fish) ->
+        if (i == generations) {
+            null
+        } else {
+            val next = mutableMapOf<Int, Long>()
+            fish.forEach { timer, count ->
+                if (timer == 0) {
+                    next.put(8, count)
+                    next.put(6, count + next.getOrDefault(6, 0L))
+                } else {
+                    next.put(timer - 1, count + next.getOrDefault(timer - 1, 0L))
+                }
+            }
+            i + 1 to next
+        }
+    }.last().second
+    
+val input = java.io.File(args[0]).readLines().first().split(",")
+val start = input.groupingBy { it.toInt() }.eachCount().mapValues { it.value.toLong() }
+println(runGenerations(start, 80).values.sum())
+println(runGenerations(start, 256).values.sum())
