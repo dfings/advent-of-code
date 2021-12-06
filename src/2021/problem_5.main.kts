bbb73a936794901aa5638086734b71199ca767ba
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-05 20:35:09 -0800

    Update problem_5.main.kts

--- src/2021/problem_5.main.kts
+++ src/2021/problem_5.main.kts
@@ -5,16 +5,12 @@ data class Line(val a: Point, val b: Point) {
     val diagonal: Boolean = a.x != b.x && a.y != b.y
     val xStep: Int = b.x.compareTo(a.x)
     val yStep: Int = b.y.compareTo(a.y)
-    val steps: Int = if (a.x == b.x) b.y - a.y else b.x - a.x
+    val steps: Int = kotlin.math.abs(if (a.x == b.x) b.y - a.y else b.x - a.x)
     val points: List<Point> = (0..steps).map { Point(a.x + xStep * it, a.y + yStep * it) }
 }
 
 fun parsePoint(raw: List<String>) = Point(raw[0].toInt(), raw[1].toInt())
-fun parseLine(raw: List<String>): Line {
-    val a = parsePoint(raw.take(2))
-    val b = parsePoint(raw.drop(2))
-    return if (a.x < b.x  || (a.x == b.x && a.y < b.y)) Line(a, b) else Line(b, a)
-}
+fun parseLine(raw: List<String>) = Line(parsePoint(raw.take(2)), parsePoint(raw.drop(2)))
 val lines = java.io.File(args[0]).readLines().map { parseLine(it.split(",", " -> ")) }
 
 val nonDiagonalPoints = lines.filterNot { it.diagonal }.flatMap { it.points }
