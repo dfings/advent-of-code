76e0fc5b2bfad9e180e7d54147409338f4243717
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-05 16:02:53 -0800

    Update problem_5.main.kts

--- src/2021/problem_5.main.kts
+++ src/2021/problem_5.main.kts
@@ -3,11 +3,11 @@
 data class Point(val x: Int, val y: Int)
 data class Line(val a: Point, val b: Point) {
     val diagonal: Boolean = a.x != b.x && a.y != b.y
-    fun contains(p: Point): Boolean = when {
-        a.x == b.x -> p.x == a.x && a.y <= p.y && p.y <= b.y
-        a.y == b.y -> p.y == a.y && a.x <= p.x && p.x <= b.x
-        a.y < b.y -> a.x <= p.x && a.y <= p.y && p.x <= b.x && p.y <= b.y && p.x - a.x == p.y - a.y
-        else -> a.x <= p.x && b.y <= p.y && p.x <= b.x && p.y <= a.y && p.x - a.x == a.y - p.y
+    val points: List<Point> get() = when {
+        a.x == b.x -> (a.y..b.y).map { Point(a.x, it) }
+        a.y == b.y -> (a.x..b.x).map { Point(it, a.y) }
+        a.y < b.y -> (0..b.x-a.x).map { Point(a.x + it, a.y + it) }
+        else -> (0..b.x-a.x).map { Point(a.x + it, a.y - it)}
     }
 }
 
@@ -18,13 +18,9 @@ fun parseLine(raw: List<String>): Line {
     return if (a.x < b.x  || (a.x == b.x && a.y < b.y)) Line(a, b) else Line(b, a)
 }
 val lines = java.io.File(args[0]).readLines().map { parseLine(it.split(",", " -> ")) }
-val nonDiagonal = lines.filterNot { it.diagonal }
 
-val maxX = lines.maxOf { it.b.x }
-val maxY = lines.maxOf { it.b.y }
-val points = (0..maxX).flatMap { x -> (0..maxY).map { y -> Point(x, y) } }
-val overlap1 = points.filter { p -> nonDiagonal.count { it.contains(p) } > 1 }
-val overlap2 = points.filter { p -> lines.count { it.contains(p) } > 1 }
+val nonDiagonalPoints = lines.filterNot { it.diagonal }.flatMap { it.points }
+println(nonDiagonalPoints.groupingBy { it }.eachCount().filterValues { it > 1 }.size)
 
-println(overlap1.size)
-println(overlap2.size)
+val allPoints = lines.flatMap { it.points }
+println(allPoints.groupingBy { it }.eachCount().filterValues { it > 1 }.size)
