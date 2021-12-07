799d28e63711168aa9f3f0c954dd04433e9cc0d8
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-07 00:16:12 -0800

    Update problem_7.main.kts

--- src/2021/problem_7.main.kts
+++ src/2021/problem_7.main.kts
@@ -6,7 +6,7 @@ val crabs = java.io.File(args[0]).readLines().first().split(",").map { it.toInt(
 val range = (crabs.minOf { it }..crabs.maxOf { it })
 
 fun linearTotalCost(x: Int) = crabs.sumOf { abs(it - x) }
-println(range.map(::linearTotalCost).minOrNull())
+println(range.minOf(::linearTotalCost))
 
 fun increasingTotalCost(x: Int) = crabs.sumOf { (1..abs(it - x)).sum() }
-println(range.map(::increasingTotalCost).minOrNull())
+println(range.minOf(::increasingTotalCost))
