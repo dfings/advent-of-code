e0bf2b87bdf2a5fb7e01b53b5de18764ed8601e8
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-08 23:24:59 -0800

    Update problem_9.main.kts

--- src/2021/problem_9.main.kts
+++ src/2021/problem_9.main.kts
@@ -1,22 +1,21 @@
 #!/usr/bin/env kotlin
 
-data class Board(val points: List<List<Int>>) {
-    val xMax = points[0].lastIndex
-    val yMax = points.lastIndex
+val board = java.io.File(args[0]).readLines().map { it.chunked(1).map { it.toInt() } }
 
-    fun height(x: Int, y: Int): Int =
-        if (x < 0 || x > xMax || y < 0 || y > yMax) 10 else points[y][x]
+val xMax = board[0].lastIndex
+val yMax = board.lastIndex
 
-    fun isLowPoint(x: Int, y: Int): Boolean {
-        val h = height(x, y)
-        return h < height(x - 1, y) && h < height(x + 1, y) &&
-               h < height(x, y - 1) && h < height(x, y + 1)
-    }
+fun height(x: Int, y: Int): Int =
+    if (x < 0 || x > xMax || y < 0 || y > yMax) 10 else board[y][x]
 
-    fun risk(x: Int, y: Int): Int = if (isLowPoint(x, y)) 1 + height(x, y) else 0
-
-    fun totalRisk() = (0..xMax).flatMap { x -> (0..yMax).map { y -> risk(x, y) } }.sum()
+fun isLowPoint(x: Int, y: Int): Boolean {
+    val h = height(x, y)
+    return h < height(x - 1, y) && h < height(x + 1, y) &&
+           h < height(x, y - 1) && h < height(x, y + 1)
 }
 
-val board = Board(java.io.File(args[0]).readLines().map { it.chunked(1).map { it.toInt() } })
-println(board.totalRisk())
+val lowPoints = (0..xMax).flatMap { 
+    x -> (0..yMax).mapNotNull { y -> if (isLowPoint(x, y)) x to y else null } 
+}
+
+println(lowPoints.sumOf { (x, y) -> 1 + height(x, y)})
