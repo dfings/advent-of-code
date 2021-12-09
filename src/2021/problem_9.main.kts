f9efa919c13d3c0ee96808ac228a0f5f638300e3
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-08 23:45:52 -0800

    Update problem_9.main.kts

--- src/2021/problem_9.main.kts
+++ src/2021/problem_9.main.kts
@@ -14,8 +14,31 @@ fun isLowPoint(x: Int, y: Int): Boolean {
            h < height(x, y - 1) && h < height(x, y + 1)
 }
 
+data class Point(val x: Int, val y: Int)
 val lowPoints = (0..xMax).flatMap { 
-    x -> (0..yMax).mapNotNull { y -> if (isLowPoint(x, y)) x to y else null } 
+    x -> (0..yMax).mapNotNull { y -> if (isLowPoint(x, y)) Point(x, y) else null } 
 }
 
-println(lowPoints.sumOf { (x, y) -> 1 + height(x, y)})
+println(lowPoints.sumOf { 1 + height(it.x, it.y)})
+
+fun basinSize(start: Point): Int {
+    val frontier = ArrayDeque<Point>()
+    val visited = mutableSetOf<Point>()
+
+    frontier.add(start)
+    while (!frontier.isEmpty()) {
+        val p = frontier.removeFirst()
+        if (height(p.x, p.y) < 9 && visited.add(p)) {
+            frontier.apply {
+                add(Point(p.x - 1, p.y))
+                add(Point(p.x + 1, p.y))
+                add(Point(p.x, p.y - 1))
+                add(Point(p.x, p.y + 1))
+            }
+        }
+    }
+
+    return visited.size
+}
+
+println(lowPoints.map(::basinSize).sorted().reversed().take(3).reduce(Int::times))
