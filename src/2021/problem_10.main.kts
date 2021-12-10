bc4e2d39b02e3380a67ce2e4ff6c86eb34e149c5
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-09 23:11:42 -0800

    Update problem_10.main.kts

--- src/2021/problem_10.main.kts
+++ src/2021/problem_10.main.kts
@@ -1,21 +1,34 @@
 #!/usr/bin/env kotlin
 
 val legalPairs = mapOf('(' to ')', '[' to ']', '{' to '}', '<' to '>')
-val scores = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
+val corruptionScores = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
+val completionScores = mapOf('(' to 1, '[' to 2, '{' to 3, '<' to 4)
 
 val lines = java.io.File(args[0]).readLines()
 
-fun String.detectCorruption(): Char? {
+sealed interface LineState
+object Ok: LineState
+class Corrupt(val score: Int): LineState
+class Incomplete(val score: Long): LineState
+
+fun String.detectCorruption(): LineState {
     val stack = ArrayDeque<Char>()
     toList().forEach {
         if (it in legalPairs.keys) {
             stack.addFirst(it)
         } else if (legalPairs[stack.removeFirst()] != it) {
-            return it
+            return Corrupt(corruptionScores.getValue(it))
         }
     }
-    return null
+    return if (stack.isEmpty()) {
+        Ok
+    } else {
+        Incomplete(stack.fold(0L) { acc, it -> acc * 5 + completionScores.getValue(it) })
+    }
 }
 
+val states = lines.map { it.detectCorruption() }
+println(states.filterIsInstance<Corrupt>().sumOf { it.score })
 
-println(lines.mapNotNull { it.detectCorruption() }.sumOf { scores.getValue(it) })
+val incompleteScores = states.filterIsInstance<Incomplete>().map { it.score }.sorted()
+println(incompleteScores[incompleteScores.size / 2])
