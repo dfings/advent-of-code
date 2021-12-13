a25c8f358e82d59e309e75e30782018c4bdad032
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-13 00:02:10 -0800

    Update problem_13.main.kts

--- src/2021/problem_13.main.kts
+++ src/2021/problem_13.main.kts
@@ -2,15 +2,9 @@
 
 data class Point(val x: Int, val y: Int)
 
-sealed interface Fold {
-    fun apply(p: Point): Point
-    data class X(val x: Int): Fold {
-        override fun apply(p: Point) = if (p.x < x) p else Point(2 * x - p.x, p.y)
-    }
-    data class Y(val y: Int): Fold {
-        override fun apply(p: Point) = if (p.y < y) p else Point(p.x, 2 * y - p.y)
-    }
-}
+typealias Fold = (Point) -> Point
+fun FoldX(x: Int): Fold = { if (it.x < x) it else Point(2 * x - it.x, it.y)}
+fun FoldY(y: Int): Fold = { if (it.y < y) it else Point(it.x, 2 * y - it.y)}
 
 fun String.toPoint() = split(',').let { Point(it[0].toInt(), it[1].toInt()) }
 
@@ -19,17 +13,17 @@ val points = lines.filter { ',' in it }.map{ it.toPoint() }
 val folds = lines.filter { it.startsWith("fold along ") }.map {
     val (command, value) = it.split("=")
     when (command) {
-        "fold along x" -> Fold.X(value.toInt())
-        "fold along y" -> Fold.Y(value.toInt())
+        "fold along x" -> FoldX(value.toInt())
+        "fold along y" -> FoldY(value.toInt())
         else -> throw IllegalStateException()
     }
 }
 
 // Part 1
-println(points.map(folds[0]::apply).distinct().size)
+println(points.map(folds[0]).distinct().size)
 
 // Part 2
-val finalPoints = folds.fold(points) { p, f -> p.map(f::apply) }.toSet()
+val finalPoints = folds.fold(points) { acc, it -> acc.map(it) }.toSet()
 val xMax = finalPoints.maxOf { it.x }
 val yMax = finalPoints.maxOf { it.y }
 for (y in (0..yMax)) {
