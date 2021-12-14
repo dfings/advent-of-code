2a360b647f5e85ee98a77ec10453b0e1c603c6f5
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-13 23:05:14 -0800

    Update problem_14.main.kts

--- src/2021/problem_14.main.kts
+++ src/2021/problem_14.main.kts
@@ -1,20 +1,27 @@
 #!/usr/bin/env kotlin
 
+fun <T> MutableMap<T, Long>.incrementKey(key: T, amount: Long) {
+    compute(key) { _, v -> (v ?: 0L) + amount }
+}
+
+fun <T> Iterable<T>.histogram(): Map<T, Long> = 
+    groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+
 fun solve(initial: String, rules: Map<String, List<String>>, rounds: Int): Long {
-    val histogram = initial.groupingBy { it }.eachCount().mapValues { it.value.toLong() }.toMutableMap()
-    var polymer = initial.windowed(2).groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+    val histogram = initial.toList().histogram().toMutableMap()
+    var polymer = initial.windowed(2).histogram().toMutableMap()
     repeat (rounds) {
-        val updated = polymer.toMutableMap()
-        for ((pair, count) in polymer) {
-            val split = rules[pair]
+        for ((pair, count) in polymer.toMap()) {
+            val split: List<String>? = rules[pair]
             if (split != null) {
-                updated.compute(pair) { _, v -> (v ?: 0) - count }
-                updated.compute(split[0]) { _, v -> (v ?: 0) + count } 
-                updated.compute(split[1]) { _, v -> (v ?: 0) + count } 
-                histogram.compute(split[0][1]) { _, v -> (v ?: 0) + count }
+                // All instances of the given pair are replaced by the 2 new pairs.
+                polymer.incrementKey(pair, -count)
+                polymer.incrementKey(split[0], count)
+                polymer.incrementKey(split[1], count)
+                // The histogram increases by the middle element.
+                histogram.incrementKey(split[0][1], count)
             }
         }
-        polymer = updated.toMap()
     }
     return histogram.maxOf { it.value } - histogram.minOf { it.value }
 }
