518fb9ef3443b103ecf89ad8a8e24e338792ceb2
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-15 23:25:27 -0800

    Update problem_16.main.kts

--- src/2021/problem_16.main.kts
+++ src/2021/problem_16.main.kts
@@ -11,47 +11,47 @@ sealed class Packet(val version: Int, val length: Int)
 class Literal(version: Int, length: Int, val value: Long) : Packet(version, length)
 class Operator(version: Int, val typeId: Int, length: Int, val subpackets: List<Packet>) : Packet(version, length)
 
-fun parsePacket(iter: Iterator<Char>): Packet {
-    val version = iter.take(3).binaryToInt()
-    val typeId = iter.take(3).binaryToInt()
+fun Iterator<Char>.parsePacket(): Packet {
+    val version = take(3).binaryToInt()
+    val typeId = take(3).binaryToInt()
     return when {
-        typeId == 4 -> parseLiteral(version, iter)
-        iter.next() == '0' -> parseLengthDelimitedOperator(version, typeId, iter)
-        else -> parsePacketDelimitedOperator(version, typeId, iter)
+        typeId == 4 -> parseLiteral(version)
+        next() == '0' -> parseLengthDelimitedOperator(version, typeId)
+        else -> parsePacketDelimitedOperator(version, typeId)
     }
 }
 
-fun parseLiteral(version: Int, iter: Iterator<Char>): Literal {
+fun Iterator<Char>.parseLiteral(version: Int): Literal {
     var blockCount = 0
     val encoded = mutableListOf<Char>()
     do {
         blockCount++
-        val partial = iter.take(5)
+        val partial = take(5)
         encoded += partial.drop(1)
     } while (partial.first() == '1')
     return Literal(version, 6 + blockCount * 5, encoded.binaryToLong())
 }
 
-fun parseLengthDelimitedOperator(version: Int, typeId: Int, iter: Iterator<Char>): Operator {
+fun Iterator<Char>.parseLengthDelimitedOperator(version: Int, typeId: Int): Operator {
     val subpackets = mutableListOf<Packet>() 
-    val length = iter.take(15).binaryToInt()
+    val length = take(15).binaryToInt()
     var remaining = length
     while (remaining > 0) {
-        subpackets.add(parsePacket(iter))
+        subpackets.add(parsePacket())
         remaining -= subpackets.last().length
     }
     return Operator(version, typeId, 22 + length, subpackets)
 }
 
-fun parsePacketDelimitedOperator(version: Int, typeId: Int, iter: Iterator<Char>): Operator {
-    val subpacketCount = iter.take(11).binaryToInt()
-    val subpackets = (1..subpacketCount).map { parsePacket(iter) }
+fun Iterator<Char>.parsePacketDelimitedOperator(version: Int, typeId: Int): Operator {
+    val subpacketCount = take(11).binaryToInt()
+    val subpackets = (1..subpacketCount).map { parsePacket() }
     return Operator(version, typeId, 18 + subpackets.sumOf { it.length }, subpackets)
 }
 
 val input = java.io.File(args[0]).readLines().single()
 val binary = input.map { it.hexToBinaryString() }.joinToString("").toList()
-val packet = parsePacket(binary.iterator())
+val packet = binary.iterator().parsePacket()
 
 fun sumVersions(packet: Packet): Int = when (packet) {
     is Literal -> packet.version
