ad7722a31f1f71e0e8d2bddfe0d2f9be6ff797f6
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-19 21:10:54 -0800

    Update problem_19.main.kts

--- src/2021/problem_19.main.kts
+++ src/2021/problem_19.main.kts
@@ -3,7 +3,7 @@
 import kotlin.math.abs
 
 data class Point(val x: Int, val y: Int, val z: Int) {
-    fun offset(o: Point): Point = Point(x - o.x, y - o.y, z - o.z)
+    operator fun minus(o: Point): Point = Point(x - o.x, y - o.y, z - o.z)
     fun offsetSignature(o: Point): Set<Int> = setOf(abs(x - o.x), abs(y - o.y), abs(z - o.z))
 }
 
@@ -20,13 +20,30 @@ class Transform(val x: Extractor, val y: Extractor, val z: Extractor): (Point) -
 }
 
 val transforms = listOf(
+    // X -> X
     Transform(X, Y, Z), Transform(X, Z, Y), Transform(X, NY, NZ), Transform(X, NZ, NY),
-    Transform(NX, Y, Z), Transform(NX, Z, Y), Transform(NX, NY, NZ), Transform(NX, NZ, NY),
-    Transform(Y, X, Z), Transform(Y, Z, X), Transform(Y, NX, NZ), Transform(Y, NZ, NX),
+    Transform(NX, NY, Z), Transform(NX, Z, NY), Transform(NX, Y, NZ), Transform(NX, NZ, NY),
+    // X -> Y
+    Transform(Y, NX, Z), Transform(Y, Z, NX), Transform(Y, X, NZ), Transform(Y, NZ, X),
     Transform(NY, X, Z), Transform(NY, Z, X), Transform(NY, NX, NZ), Transform(NY, NZ, NX),
-    Transform(Z, Y, X), Transform(Z, X, Y), Transform(Z, NY, NX), Transform(Z, NX, NY),
-    Transform(NZ, Y, X), Transform(NZ, X, Y), Transform(NZ, NY, NX), Transform(NZ, NX, NY),
+    // X -> Z
+    Transform(Z, X, Y), Transform(Z, Y, X), Transform(Z, NX, NY), Transform(Z, NY, NX),
+    Transform(NZ, NX, Y), Transform(NZ, Y, NX), Transform(NZ, X, NY), Transform(NZ, NY, X),
 )
+/*
+
+val transforms = listOf(
+    // X -> X
+    Transform(X, Y, Z), Transform(X, Z, Y), Transform(X, NY, NZ), Transform(X, NZ, NY),
+    Transform(NX, NY, Z), Transform(NX, Z, NY), Transform(NX, Y, NZ), Transform(NX, NZ, NY),
+    // X -> Y
+    Transform(NY, X, Z), Transform(Y, X, NZ), Transform(Z, X, NY), Transform(NZ, X, Y),
+    Transform(Y, NX, Z), Transform(Z, NX, Y), Transform(NY, NX, NZ), Transform(NZ, NX, NY),
+    // X -> Z
+    Transform(Y, Z, X), Transform(Z, Y, X), Transform(NY, NZ, X), Transform(NZ, NY, X),
+    Transform(NY, Z, NX), Transform(Z, NY, NX), Transform(Y, NZ, NX), Transform(NZ, Y, X),
+)
+ */
 
 data class Scanner(val index: Int, val beacons: List<Point>) {
     val signatures: Map<Set<Int>, Pair<Point, Point>> =
@@ -37,6 +54,9 @@ data class Scanner(val index: Int, val beacons: List<Point>) {
     fun commonSignatures(o: Scanner) = (signatures.keys intersect o.signatures.keys)
     // 66 = 12 * (12 - 1) / 2
     fun overlaps(o: Scanner) = commonSignatures(o).size >= 66
+
+    fun normalize(transform: Transform, offset: Point) =
+        Scanner(index, beacons.map { transform(it) - offset })
 }
 
 fun Iterator<String>.parseScanner(): Scanner {
@@ -64,18 +84,30 @@ val scanners = buildList {
 }
 
 val overlaps = scanners.flatMap { a -> scanners.map { b -> a to b } }
-    .filter { (a, b) -> a.index < b.index && a.overlaps(b) }
+    .filter { (a, b) -> a.index != b.index && a.overlaps(b) }
     .map { (a, b) -> a.index to b.index }
-assert(overlaps.flatMap { it.toList() }.toSet().size == scanners.size)
-println(overlaps.joinToString("\n"))
+    .groupBy { it.first }
+    .mapValues { it.value.map { it.second } }
+
+println(overlaps)
 
 val scanner0 = scanners[0]
 val scanner2 = scanners[2]
 val commonSignatures = scanner0.commonSignatures(scanner2)
 val pair1 = scanner0.signatures.getValue(commonSignatures.first())
 val pair2 = scanner2.signatures.getValue(commonSignatures.first())
-val potentialTransforms = transforms.filter { 
-    pair1.first.offset(pair1.second) == it(pair2.first).offset(it(pair2.second)) ||
-    pair1.first.offset(pair1.second) == it(pair2.second).offset(it(pair2.first))
+val transform = transforms.single { 
+    pair1.first - pair1.second == it(pair2.first) - it(pair2.second) ||
+    pair1.first - pair1.second == it(pair2.second) - it(pair2.first)
 }
-println(potentialTransforms.size)
+val scannerOffset =
+    if (pair1.first - pair1.second == transform(pair2.first) - transform(pair2.second)) {
+        check(transform(pair2.first) - pair1.first == transform(pair2.second) - pair1.second)
+        transform(pair2.first) - pair1.first
+    } else {
+        check(transform(pair2.second) - pair1.first == transform(pair2.first) - pair1.second)
+        transform(pair2.second) - pair1.first
+    }
+println(scannerOffset)
+val nscanner2 = scanner2.normalize(transform, scannerOffset)
+println((scanner0.beacons.toSet() intersect nscanner2.beacons.toSet()).size)
