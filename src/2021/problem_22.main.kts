2e6071719328f636e6b4877b51e05e5ce1d3e5f0
Dan Fingal-Surma <dfingal@gmail.com>
2021-12-22 15:55:56 -0800

    Update problem_22.main.kts

--- src/2021/problem_22.main.kts
+++ src/2021/problem_22.main.kts
@@ -25,3 +25,37 @@ instructions.forEach { instruction ->
     toUpdate.forEach { states.put(it, instruction.on) }
 }
 println(states.values.count { it })
+
+fun IntRange.end() = endInclusive + 1
+val sectors = instructions.map { it.sector }
+val xs = sectors.flatMap { listOf(it.x.start, it.x.end()) }.sorted().distinct()
+val ys = sectors.flatMap { listOf(it.y.start, it.y.end()) }.sorted().distinct()
+val zs = sectors.flatMap { listOf(it.z.start, it.z.end()) }.sorted().distinct()
+
+val xIndex = xs.mapIndexed { index, value -> value to index }.toMap()
+val yIndex = ys.mapIndexed { index, value -> value to index }.toMap()
+val zIndex = zs.mapIndexed { index, value -> value to index }.toMap()
+
+val bits = Array(xs.size) { Array(ys.size) { BooleanArray(zs.size) { false } } }
+for (instruction in instructions) {
+    val s = instruction.sector
+    for (x in xIndex.getValue(s.x.start) until xIndex.getValue(s.x.end())) {
+        for (y in yIndex.getValue(s.y.start) until yIndex.getValue(s.y.end())) {
+            for (z in zIndex.getValue(s.z.start) until zIndex.getValue(s.z.end())) {
+                bits[x][y][z] = instruction.on
+            }
+        }
+    }
+}
+
+var volume = 0L
+for (x in 0 until xs.lastIndex) {
+    for (y in 0 until ys.lastIndex) {
+        for (z in 0 until zs.lastIndex) {
+            if (bits[x][y][z]) {
+                volume += 1L * (xs[x+1]-xs[x]) * (ys[y+1]-ys[y]) * (zs[z+1]-zs[z])
+            }
+        }
+    }
+}
+println(volume)
