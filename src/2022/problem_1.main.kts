8d14e1cd8ce019207426be31e970d50c9c920268
Dan Fingal-Surma <dfingal@gmail.com>
2022-11-30 21:21:54 -0800

    Create problem_1.main.kts

---
+++ src/2022/problem_1.main.kts
@@ -0,0 +1,14 @@
+#!/usr/bin/env kotlin
+
+val data = java.io.File(args[0]).readLines()
+
+val calorieCount = mutableListOf(0)
+data.forEach { line ->
+  if (line.isBlank()) {
+    calorieCount.add(0)
+  } else {
+    calorieCount[calorieCount.lastIndex] += line.toInt()
+  }
+}
+println(calorieCount.max())
+println(calorieCount.sortedBy { -it}.take(3).sum())
