c6e1300a90c3dc30538f3403b155baddaa2f4c6c
Dan Fingal-Surma <dfingal@gmail.com>
2022-11-30 21:27:33 -0800

    Run ktlint

--- src/2022/problem_1.main.kts
+++ src/2022/problem_1.main.kts
@@ -4,11 +4,11 @@ val data = java.io.File(args[0]).readLines()
 
 val calorieCount = mutableListOf(0)
 data.forEach { line ->
-  if (line.isBlank()) {
-    calorieCount.add(0)
-  } else {
-    calorieCount[calorieCount.lastIndex] += line.toInt()
-  }
+    if (line.isBlank()) {
+        calorieCount.add(0)
+    } else {
+        calorieCount[calorieCount.lastIndex] += line.toInt()
+    }
 }
 println(calorieCount.max())
-println(calorieCount.sortedBy { -it}.take(3).sum())
+println(calorieCount.sortedBy { -it }.take(3).sum())
