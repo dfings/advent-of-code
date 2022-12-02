173313b17b3145e4aafbfaa55f83e7b4ed80577c
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-01 23:18:44 -0800

    Reorder cases

--- src/2022/problem_2.main.kts
+++ src/2022/problem_2.main.kts
@@ -4,15 +4,15 @@ val pairs = java.io.File(args[0]).readLines().map { it.split(" ").map { it[0] } 
 
 // Rock = 0, Paper = 1, Scissors = 2
 fun score1(opponentPlay: Int, selfPlay: Int): Int = 1 + selfPlay + when (selfPlay) {
-    opponentPlay -> 3 // Draw
     (opponentPlay + 1) % 3 -> 6 // Win
+    opponentPlay -> 3 // Draw
     else -> 0 // Lose
 }
 
 fun score2(opponentPlay: Int, outcome: Char): Int = 1 + when (outcome) {
     'Z' -> (opponentPlay + 1) % 3 + 6 // Win
-    'X' -> (opponentPlay + 2) % 3 // Lose
-    else -> opponentPlay + 3 // Draw
+    'Y' -> opponentPlay + 3 // Draw
+    else -> (opponentPlay + 2) % 3 // Lose
 }
 
 println(pairs.sumBy { score1(it[0] - 'A', it[1] - 'X') })
