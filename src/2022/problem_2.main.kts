efd46e0e529a1ee14c29b0febac4597072ad79af
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-01 23:16:45 -0800

    Update problem_2.main.kts

--- src/2022/problem_2.main.kts
+++ src/2022/problem_2.main.kts
@@ -9,14 +9,11 @@ fun score1(opponentPlay: Int, selfPlay: Int): Int = 1 + selfPlay + when (selfPla
     else -> 0 // Lose
 }
 
-fun score2(opponentPlay: Int, outcome: Char): Int = score1(
-    opponentPlay,
-    when (outcome) {
-        'Z' -> (opponentPlay + 1) % 3 // Win
-        'X' -> (opponentPlay + 2) % 3 // Lose
-        else -> opponentPlay // Draw
-    }
-)
+fun score2(opponentPlay: Int, outcome: Char): Int = 1 + when (outcome) {
+    'Z' -> (opponentPlay + 1) % 3 + 6 // Win
+    'X' -> (opponentPlay + 2) % 3 // Lose
+    else -> opponentPlay + 3 // Draw
+}
 
 println(pairs.sumBy { score1(it[0] - 'A', it[1] - 'X') })
 println(pairs.sumBy { score2(it[0] - 'A', it[1]) })
