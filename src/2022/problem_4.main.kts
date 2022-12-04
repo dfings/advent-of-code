dfb8f8a2acec1f93d461ec79632337db37070fe0
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-03 23:05:43 -0800

    Simplify part 2

--- src/2022/problem_4.main.kts
+++ src/2022/problem_4.main.kts
@@ -4,7 +4,7 @@ val lines = java.io.File(args[0]).readLines()
 val values = lines.map { it.split("-", ",").map { it.toInt() } }
 
 val part1 = values.filter { (a, b, c, d) -> (a <= c && b >= d) || (a >= c && b <= d) }.count()
-val part2 = values.filter { (a, b, c, d) -> (a <= c && b >= c) || (a >= c && a <= d) }.count()
+val part2 = values.filter { (a, b, c, d) -> a <= d && b >= c }.count()
 
 println(part1)
 println(part2)
