a05eb65a3ec72a8d8e87b860323410c6bd7eff3c
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-03 23:08:07 -0800

    Update problem_4.main.kts

--- src/2022/problem_4.main.kts
+++ src/2022/problem_4.main.kts
@@ -3,8 +3,8 @@
 val lines = java.io.File(args[0]).readLines()
 val values = lines.map { it.split("-", ",").map { it.toInt() } }
 
-val part1 = values.filter { (a, b, c, d) -> (a <= c && b >= d) || (a >= c && b <= d) }.count()
-val part2 = values.filter { (a, b, c, d) -> a <= d && b >= c }.count()
+val part1 = values.count { (a, b, c, d) -> (a <= c && b >= d) || (a >= c && b <= d) }
+val part2 = values.count { (a, b, c, d) -> a <= d && b >= c }
 
 println(part1)
 println(part2)
