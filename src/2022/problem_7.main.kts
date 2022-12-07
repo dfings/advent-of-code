f42649656fe92184bd120c35dbbff0e28dd6cd63
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-06 22:37:05 -0800

    Update problem_7.main.kts

--- src/2022/problem_7.main.kts
+++ src/2022/problem_7.main.kts
@@ -1,6 +1,6 @@
 #!/usr/bin/env kotlin
 
-class Dir(val name: String, val parent: Dir? = null) {
+class Dir(val parent: Dir? = null) {
     val dirs: MutableList<Dir> = mutableListOf()
     var fileSize: Int = 0
     val size: Int by lazy {
@@ -9,23 +9,21 @@ class Dir(val name: String, val parent: Dir? = null) {
 }
 
 val lines = java.io.File(args[0]).readLines().drop(1)
-val root = Dir("/")
-var cwd = root
-val allDirs = mutableListOf<Dir>(root)
+var cwd = Dir()
+val allDirs = mutableListOf<Dir>(cwd)
 for (line in lines) {
   when {
-    line.startsWith("$ ls") -> {}
-    line.startsWith("dir ") -> Dir(line.drop(4), cwd).apply {
-        cwd.dirs.add(this)
-        allDirs.add(this)
-    }
     line[0].isDigit() -> cwd.fileSize += line.split(" ").first().toInt()
     line.startsWith("$ cd ..") -> cwd = cwd.parent!!
-    line.startsWith("$ cd ") -> cwd = cwd.dirs.find { it.name == line.drop(5) }!!
+    line.startsWith("$ cd ") -> Dir(cwd).apply {
+        cwd.dirs.add(this)
+        allDirs.add(this)
+        cwd = this
+    }
   }
 }
 
 println(allDirs.filter { it.size <= 100000 }.sumBy { it.size })
 
-val amountToDelete = 30000000 - (70000000 - root.size)
+val amountToDelete = 30000000 - (70000000 - allDirs.first().size)
 println(allDirs.sortedBy { it.size }.find { it.size >= amountToDelete }?.size)
