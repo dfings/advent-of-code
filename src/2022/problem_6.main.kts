2592dd41eb6ba49094f2546f3286eaf5bcc40714
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-06 20:03:30 -0800

    Update problem_6.main.kts

--- src/2022/problem_6.main.kts
+++ src/2022/problem_6.main.kts
@@ -3,7 +3,7 @@
 val datastream = java.io.File(args[0]).readLines().single()
 
 fun String.detect(windowSize: Int): Int = 
-    windowed(windowSize).indexOfFirst { it.toSet().size == windowSize } + windowSize
+    windowedSequence(windowSize).indexOfFirst { it.toSet().size == windowSize } + windowSize
 
 println(datastream.detect(4))
 println(datastream.detect(14))
