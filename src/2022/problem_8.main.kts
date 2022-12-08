265750df8225fc9d69e3f3e71171bcf6ef3cb7f8
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-07 23:13:33 -0800

    Update problem_8.main.kts

--- src/2022/problem_8.main.kts
+++ src/2022/problem_8.main.kts
@@ -3,16 +3,13 @@
 class Grid(heights: List<List<Int>>) {
     val xMax = heights[0].lastIndex
     val yMax = heights.lastIndex
-    val points = heights.mapIndexed { y, it -> it.mapIndexed { x, it -> Point(x, y, it) } }
-    val pointsList = (0..xMax).flatMap { x -> (0..yMax).map { y -> points[y][x] } }
-
-    fun point(x: Int, y: Int) = points[y][x]
+    val points = (0..xMax).map { x -> (0..yMax).map { y -> Point(x, y, heights[y][x]) } }
 
     inner class Point(val x: Int, val y: Int, val height: Int) {
-        fun left() = (x - 1 downTo 0).map { point(it, y) }
-        fun right() = (x + 1..xMax).map { point(it, y) }
-        fun up() = (y - 1 downTo 0).map { point(x, it) }
-        fun down() = (y + 1..yMax).map { point(x, it) }
+        fun left() = (x - 1 downTo 0).map { points[it][y] }
+        fun right() = (x + 1..xMax).map { points[it][y] }
+        fun up() = (y - 1 downTo 0).map { points[x][it] }
+        fun down() = (y + 1..yMax).map { points[x][it] }
 
         fun isVisible(): Boolean {
             fun List<Point>.visible() = all { it.height < height }
@@ -29,5 +26,5 @@ class Grid(heights: List<List<Int>>) {
 
 val lines = java.io.File(args[0]).readLines()
 val grid: Grid = Grid(lines.map { it.map { "$it".toInt() } })
-println(grid.pointsList.count { it.isVisible() })
-println(grid.pointsList.maxOf { it.score() })
+println(grid.points.flatMap { it }.count { it.isVisible() })
+println(grid.points.flatMap { it }.maxOf { it.score() })
