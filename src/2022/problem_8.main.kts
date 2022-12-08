da553c8049ea04641a8617ad10a8cf01190e010b
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-07 22:21:07 -0800

    Update problem_8.main.kts

--- src/2022/problem_8.main.kts
+++ src/2022/problem_8.main.kts
@@ -1,55 +1,42 @@
 #!/usr/bin/env kotlin
 
 val lines = java.io.File(args[0]).readLines()
-val grid = lines.map { it.map { "$it".toInt() } }
 
-fun List<List<Int>>.isVisible(x: Int, y: Int): Boolean {
-    val height = this[y][x]
-    return (0..x - 1).all { this[y][it] < height } ||
-        (x + 1..this[y].lastIndex).all { this[y][it] < height } ||
-        (0..y - 1).all { this[it][x] < height } ||
-        (y + 1..lastIndex).all { this[it][x] < height }
+data class Point(val x: Int, val y: Int)
+data class Grid(val heights: List<List<Int>>) {
+    val xMax = heights[0].lastIndex
+    val yMax = heights.lastIndex
+    val points = (0..xMax).flatMap { x -> (0..yMax).map { y -> Point(x, y) } }
+    fun height(x: Int, y: Int) = heights[y][x]
+    fun height(p: Point) = height(p.x, p.y)
 }
 
-var count = 0
-for (y in 0..grid.lastIndex) {
-    for (x in 0..grid[y].lastIndex) {
-        if (grid.isVisible(x, y)) count++
-    }
+fun Grid.isVisible(x: Int, y: Int): Boolean {
+    fun isLower(x2: Int, y2: Int) = height(x2, y2) < height(x, y)
+    return (0..x - 1).all { isLower(it, y) } ||
+        (x + 1..xMax).all { isLower(it, y) } ||
+        (0..y - 1).all { isLower(x, it) } ||
+        (y + 1..yMax).all { isLower(x, it) }
 }
 
-println(count)
-
-fun List<List<Int>>.score(x: Int, y: Int): Int {
-    val height = this[y][x]
-    var score1 = 0
-    for (i in x - 1 downTo 0) {
-        score1++
-        if (this[y][i] >= height) break
+fun Grid.score(x: Int, y: Int): Int {
+    fun isLower(p: Point) = height(p) < height(x, y)
+    fun countVisible(points: List<Point>): Int {
+        var count = 0
+        for (point in points) {
+            count++
+            if (!isLower(point)) break
+        }
+        return count
     }
-    var score2 = 0
-    for (i in x + 1..this[y].lastIndex) {
-        score2++
-        if (this[y][i] >= height) break
-    }
-    var score3 = 0
-    for (j in y - 1 downTo 0) {
-        score3++
-        if (this[j][x] >= height) break
-    }
-    var score4 = 0
-    for (j in y + 1..lastIndex) {
-        score4++
-        if (this[j][x] >= height) break
-    }
+    val score1 = countVisible((x - 1 downTo 0).map { Point(it, y) })
+    val score2 = countVisible((x + 1..xMax).map { Point(it, y) })
+    val score3 = countVisible((y - 1 downTo 0).map { Point(x, it) })
+    val score4 = countVisible((y + 1..yMax).map { Point(x, it) })
     return score1 * score2 * score3 * score4
 }
 
-val scores = mutableListOf<Int>()
-for (y in 0..grid.lastIndex) {
-    for (x in 0..grid[y].lastIndex) {
-        scores.add(grid.score(x, y))
-    }
-}
+val grid: Grid = Grid(lines.map { it.map { "$it".toInt() } })
 
-println(scores.max())
+println(grid.points.count { grid.isVisible(it.x, it.y) })
+println(grid.points.maxOf { grid.score(it.x, it.y) })
