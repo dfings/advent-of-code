38ef6eb5bf0402f9581235aef0dfb75ff34d52c2
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-10 23:33:22 -0800

    Create problem_11.main.kts

---
+++ src/2022/problem_11.main.kts
@@ -0,0 +1,58 @@
+#!/usr/bin/env kotlin
+
+class Item(var worry: Long)
+class Monkey(val items: MutableSet<Item>, val op: (Long) -> Long, val test: Long, val reduceWorry: Boolean) {
+    lateinit var trueMonkey: Monkey
+    lateinit var falseMonkey: Monkey
+    var count = 0L
+
+    fun inspectAll() = items.toList().forEach(this::inspect)
+    fun inspect(item: Item) {
+        count++
+        item.worry = op(item.worry)
+        if (reduceWorry) {
+            item.worry /= 3
+        }
+        val passTo = if (item.worry % test == 0L) trueMonkey else falseMonkey
+        passTo.items.add(item)
+        items.remove(item)
+    }
+}
+
+fun parseOp(code: String, rhs: String): (Long) -> Long {
+    if (rhs == "old") {
+        return { input: Long -> input * input }
+    } 
+    val value = rhs.toLong()
+    if (code == "+") {
+        return { input: Long -> input + value }
+    }
+    return { input: Long -> input * value }
+}
+
+fun runSimulation(iterations: Int, reduceWorry: Boolean) {
+    val lines = java.io.File(args[0]).readLines()
+    val monkeys = mutableListOf<Monkey>()
+    val allItems = mutableListOf<Item>()
+    for (spec in lines.chunked(7)) {
+        val items = spec[1].drop(18).split(", ").map { Item(it.toLong()) }
+        val op: (Long) -> Long = spec[2].drop(23).split(" ").let { (code, rhs) -> parseOp(code, rhs) }
+        val test = spec[3].drop(21).toLong()
+        monkeys.add(Monkey(items.toMutableSet(), op, test, reduceWorry))
+        allItems.addAll(items)
+    }
+    lines.chunked(7).forEachIndexed { i, spec ->
+        monkeys[i].trueMonkey = monkeys[spec[4].drop(29).toInt()]
+        monkeys[i].falseMonkey = monkeys[spec[5].drop(30).toInt()]
+    }
+
+    val scale = monkeys.map { it.test }.fold(1L) { acc, it -> acc * it }
+    repeat(iterations) {
+        monkeys.forEach { it.inspectAll() }
+        allItems.forEach { it.worry = it.worry % scale }
+    }
+    println(monkeys.map { it.count }.sorted().takeLast(2).reduce(Long::times))
+}
+
+runSimulation(iterations = 20, reduceWorry = true)
+runSimulation(iterations = 10000, reduceWorry = false)
