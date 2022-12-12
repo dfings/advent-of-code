3ef8bce9caefc73c764a854869cf614ca88d3b70
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-11 23:41:47 -0800

    Update problem_12.main.kts

--- src/2022/problem_12.main.kts
+++ src/2022/problem_12.main.kts
@@ -1,6 +1,6 @@
 #!/usr/bin/env kotlin
 
-class Vertex(val x: Int, val y: Int, val code: Char, var distance: Int = Int.MAX_VALUE) {
+class Vertex(val x: Int, val y: Int, val code: Char) {
     val height: Int
       get() = when(code) {
         'S' -> 0
@@ -13,23 +13,26 @@ class Graph(val vertexes: List<List<Vertex>>) {
     val xMax = vertexes[0].lastIndex
     val yMax = vertexes.lastIndex
 
-    fun computeShortestPaths() {
+    fun findShortedPath(target: Char): Int? {
         val end = vertexes.flatten().single { it.code == 'E' }
-        end.distance = 0
-        val frontier = ArrayDeque<Vertex>(listOf(end))
+        val frontier = ArrayDeque<Pair<Vertex, Int>>()
+        val visited = mutableSetOf<Vertex>()
+        frontier.add(end to 0)
         while (!frontier.isEmpty()) {
-            val vertex = frontier.removeFirst()
-            vertex.neighbors().filter { it.distance == Int.MAX_VALUE }.forEach { 
-                it.distance = vertex.distance + 1
-                frontier.add(it)
+            val (vertex, distance) = frontier.removeFirst()
+            if (!visited.add(vertex)) continue
+            if (vertex.code == target) return distance
+            vertex.neighbors().filter { it.height + 1 >= vertex.height }.forEach { 
+                frontier.add(it to distance + 1)
             }
         }
+        return null
     }
 
     fun Vertex.neighbors() = listOfNotNull(
         vertexAt(x - 1, y), vertexAt(x + 1, y), 
         vertexAt(x, y - 1), vertexAt(x, y + 1)
-    ).filter { it.height + 1 >= height }
+    )
     
     fun vertexAt(x: Int, y: Int) =
         if (x < 0 || x > xMax || y < 0 || y > yMax) null else vertexes[y][x]
@@ -38,6 +41,5 @@ class Graph(val vertexes: List<List<Vertex>>) {
 val lines = java.io.File(args[0]).readLines()
 val graph = Graph(lines.mapIndexed { y, line -> line.mapIndexed { x, code -> Vertex(x, y, code) } })
 
-graph.computeShortestPaths()
-println(graph.vertexes.flatten().single { it.code == 'S' }.distance)
-println(graph.vertexes.flatten().filter{ it.height == 0}.minOf { it.distance })
+println(graph.findShortedPath('S'))
+println(graph.findShortedPath('a'))
