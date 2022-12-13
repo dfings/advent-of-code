ef09a68bfab663e27e4e55d94a995aa9e5668bcc
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-13 00:12:12 -0800

    Update problem_13.main.kts

--- src/2022/problem_13.main.kts
+++ src/2022/problem_13.main.kts
@@ -5,11 +5,10 @@ sealed interface PacketData : Comparable<PacketData>
 data class ListValue(val values: List<PacketData>) : PacketData {
     override fun toString() = values.toString()
     override operator fun compareTo(other: PacketData): Int = when (other) {
-            is ListValue -> values.zip(other.values).asSequence()
-                .map { (a, b) -> a.compareTo(b) }
-                .firstOrNull { it != 0 } 
-                ?: values.size.compareTo(other.values.size)
-            else -> compareTo(ListValue(listOf(other)))
+        is ListValue -> values.zip(other.values).asSequence()
+            .map { (a, b) -> a.compareTo(b) }
+            .firstOrNull { it != 0 } ?: values.size.compareTo(other.values.size)
+        else -> compareTo(ListValue(listOf(other)))
     }
 }
 
@@ -21,28 +20,26 @@ data class IntValue(val value: Int) : PacketData {
     }
 }
 
-fun String.parsePacketData(): PacketData {
-    if (!startsWith("[")) return IntValue(toInt())
-    val innerData = mutableListOf<PacketData>()
-    val remaining = ArrayDeque<Char>(drop(1).toList())
-    while (remaining.size > 1) {
-        val innerLine = mutableListOf<Char>()
+fun String.parsePacketData() = ArrayDeque(toList()).parsePacketData()
+fun ArrayDeque<Char>.parsePacketData(): PacketData {
+    if (first() != '[') return IntValue(joinToString("").toInt())
+    removeFirst()
+    val values = mutableListOf<PacketData>()
+    while (size > 1) {
+        val text = ArrayDeque<Char>()
         var braceCounter = 0
         while (true) {
-            val character = remaining.removeFirst()!!
+            val character = removeFirst()!!
             when (character) {
                 '[' -> braceCounter++
-                ']' -> {
-                    braceCounter--
-                    if (braceCounter < 0) break
-                }
+                ']' -> if (--braceCounter < 0) break
                 ',' -> if (braceCounter == 0) break
             }
-            innerLine.add(character)
+            text.add(character)
         }
-        innerData.add(innerLine.joinToString("").parsePacketData())
+        values.add(text.parsePacketData())
     }
-    return ListValue(innerData)
+    return ListValue(values)
 }
 
 val lines = java.io.File(args[0]).readLines()
