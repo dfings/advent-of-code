f09c8a8427d182ec40cf57f58a85877aa8c6460b
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-13 23:16:28 -0800

    Update problem_14.main.kts

--- src/2022/problem_14.main.kts
+++ src/2022/problem_14.main.kts
@@ -9,8 +9,9 @@ data class Cave(val points: MutableMap<Point, Material>, val hasFloor: Boolean) 
     val source = Point(500, 0)
     val yMax = points.keys.maxOf { it.y } + if (hasFloor) 2 else 0
 
-    fun dropAll() {
+    fun dropAll(): Int {
         while (dropNext()) {}
+        return points.values.count { it == Material.SAND }
     }
 
     fun dropNext(): Boolean {
@@ -26,34 +27,34 @@ data class Cave(val points: MutableMap<Point, Material>, val hasFloor: Boolean) 
         return false
     }
 
-    fun Point.next() = when {
-        Point(x, y + 1) !in points -> Point(x, y + 1)
-        Point(x - 1, y + 1) !in points -> Point(x - 1, y + 1)
-        Point(x + 1, y + 1) !in points -> Point(x + 1, y + 1)
-        else -> this
+    val dxs = listOf(0, -1, 1)
+    fun Point.next() : Point {
+        for (dx in dxs) {
+            Point(x + dx, y + 1).let { if (it !in points) return it }
+        }
+        return this
     }
 }
 
-fun expand(a: Point, b: Point) = if (a.x == b.x) {
-    (min(a.y, b.y)..max(a.y, b.y)).map { Point(a.x, it) }
+fun Point.to(other: Point) = if (x == other.x) {
+    (min(y, other.y)..max(y, other.y)).map { Point(x, it) }
 } else {
-    (min(a.x, b.x)..max(a.x, b.x)).map { Point(it, a.y) }
+    (min(x, other.x)..max(x, other.x)).map { Point(it, y) }
 }
 
 fun String.toPoint() = split(",").map { it.toInt() }.let { Point(it[0], it[1]) }
-fun String.toLine() = split(" -> ").map { it.toPoint() }.windowed(2).map { expand(it[0], it[1]) }.flatten()
+fun String.toPoints() = split(" -> ").map { it.toPoint() }.windowed(2).map { it[0].to(it[1]) }.flatten()
 
 val lines = java.io.File(args[0]).readLines()
-val points = lines.flatMap { it.toLine() }.map { it to Material.ROCK }.toMap()
+val points = lines.flatMap { it.toPoints() }.map { it to Material.ROCK }.toMap()
 
 val abyssCave = Cave(points.toMutableMap(), hasFloor = false)
-abyssCave.dropAll()
-println(abyssCave.points.values.count { it == Material.SAND })
+println(abyssCave.dropAll())
 
 val floorCave = Cave(points.toMutableMap(), hasFloor = true)
-floorCave.dropAll()
-println(floorCave.points.values.count { it == Material.SAND })
+println(floorCave.dropAll())
 
+// Debug printing.
 fun Cave.print() {
     for (y in points.keys.minOf { it.y }..points.keys.maxOf { it.y }) {
         val line = (points.keys.minOf { it.x }..points.keys.maxOf { it.x }).map { x ->
