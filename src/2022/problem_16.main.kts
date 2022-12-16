87faf84cbaf956c01de2c689ed5494932b381461
Dan Fingal-Surma <dfingal@gmail.com>
2022-12-16 14:41:46 -0800

    Update problem_16.main.kts

--- src/2022/problem_16.main.kts
+++ src/2022/problem_16.main.kts
@@ -32,51 +32,53 @@ class Graph(valves: List<Valve>) {
     }
 }
 
-class AgentState(val start: Valve, val timeLimit: Int) {
+class Agent(val start: Valve, val timeLimit: Int) {
     var minute = 0
     val remainingTime: Int get() = timeLimit - minute
     var currentPosition: Valve = start
 }
 
 class Solver(graph: Graph, val timeLimit: Int, val agentCount: Int) {
-    val agents = List<AgentState>(agentCount) { AgentState(graph.start, timeLimit) }
+    val agents = List<Agent>(agentCount) { Agent(graph.start, timeLimit) }
+    var agentIndex = 0
+    val agent: Agent get() = agents[agentIndex]
 
     val opened = HashSet<Valve>()
     var currentScore = 0
     var bestScore = 0
 
     fun computeMaxFlow() {
-        recursivelyTryToMove(0)
+        recursivelyTryToMove()
     }
 
-    private fun recursivelyTryToMove(agent: Int) {
-        val state = agents[agent]
+    private fun recursivelyTryToMove() {
         if (opened.size == graph.valveCount) {
             bestScore = max(bestScore, currentScore)
             return
         }
 
-        if (state.minute == timeLimit) {
-            if (agent == agents.lastIndex) {
+        if (agent.minute == timeLimit) {
+            if (agentIndex == agents.lastIndex) {
                 bestScore = max(bestScore, currentScore)
             } else {
-                recursivelyTryToMove(agent + 1)
+                agentIndex++
+                recursivelyTryToMove()
+                agentIndex--
             }
             return
         }
 
-        if (cannotWin(agent)) return
+        if (cannotWin()) return
 
-        for (move in graph.moves.getValue(state.currentPosition)) {
-            recursivelyTryToMoveTo(agent, move.valve, move.length)
+        for (move in graph.moves.getValue(agent.currentPosition)) {
+            recursivelyTryToMoveTo(move.valve, move.length)
         }
-        recursivelyTryToMoveTo(agent, graph.start, state.remainingTime)
+        recursivelyTryToMoveTo(graph.start, agent.remainingTime)
     }
 
-    private fun cannotWin(agent: Int): Boolean {
-        val state = agents[agent]
+    private fun cannotWin(): Boolean {
         var maxScore = currentScore
-        if (agent < agents.lastIndex) {
+        if (agentIndex < agents.lastIndex) {
             for (i in graph.valvesByFlow.indices) {
                 if (graph.valvesByFlow[i] in opened) continue
                 maxScore += graph.valvesByFlow[i].flow * timeLimit
@@ -85,30 +87,29 @@ class Solver(graph: Graph, val timeLimit: Int, val agentCount: Int) {
             var index = 0
             for (i in graph.valvesByFlow.indices) {
                 if (graph.valvesByFlow[i] in opened) continue
-                maxScore += graph.valvesByFlow[i].flow * (state.remainingTime - index)
+                maxScore += graph.valvesByFlow[i].flow * (agent.remainingTime - index)
                 index++
-                if (state.minute + index >= timeLimit) break
+                if (agent.minute + index >= timeLimit) break
             }
         }
         return maxScore < bestScore
     }
 
-    private fun recursivelyTryToMoveTo(agent: Int, valve: Valve, length: Int) {
-        val state = agents[agent]
-        if (state.minute + length > timeLimit) return
+    private fun recursivelyTryToMoveTo( valve: Valve, length: Int) {
+        if (agent.minute + length > timeLimit) return
 
         if (valve != graph.start && !opened.add(valve)) return
-        state.minute += length
-        val delta = (state.remainingTime) * valve.flow
+        agent.minute += length
+        val delta = (agent.remainingTime) * valve.flow
         currentScore += delta
-        val lastPosition = state.currentPosition
-        state.currentPosition = valve
+        val lastPosition = agent.currentPosition
+        agent.currentPosition = valve
 
-        recursivelyTryToMove(agent)
+        recursivelyTryToMove()
 
-        state.currentPosition = lastPosition
+        agent.currentPosition = lastPosition
         currentScore -= delta
-        state.minute -= length
+        agent.minute -= length
         if (valve != graph.start) opened.remove(valve)
     }
 }
