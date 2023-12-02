aa13a1ccb18d2ec8b04ddf618a59e14498576c37
Dan Fingal-Surma <dfingal@gmail.com>
2023-12-01 21:38:27 -0800

    Update problem_2.main.kts

--- src/2023/problem_2.main.kts
+++ src/2023/problem_2.main.kts
@@ -4,13 +4,11 @@ val lines = java.io.File(args[0]).readLines()
 
 data class Reveal(val red: Int, val green: Int, val blue: Int)
 
+fun List<List<String>>.colorCount(color: String) = find { it[1] == color }?.get(0)?.toInt() ?: 0
+
 fun String.toReveal(): Reveal {
     val pulls = trim().split(", ").map { it.split(' ') }
-    return Reveal(
-        pulls.find { it[1] == "red" }?.get(0)?.toInt() ?: 0,
-        pulls.find { it[1] == "green" }?.get(0)?.toInt() ?: 0,
-        pulls.find { it[1] == "blue" }?.get(0)?.toInt() ?: 0,
-    )
+    return Reveal(pulls.colorCount("red"), pulls.colorCount("green"), pulls.colorCount("blue"))
 }
 
 val games = lines.map { it.split(':', ';').drop(1).map { it.toReveal() } }
