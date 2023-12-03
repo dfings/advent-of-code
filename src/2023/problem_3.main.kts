498a9bf43a492fddb5c81c6159a8def759894cff
Dan Fingal-Surma <dfingal@gmail.com>
2023-12-03 00:17:50 -0800

    Update problem_3.main.kts

--- src/2023/problem_3.main.kts
+++ src/2023/problem_3.main.kts
@@ -3,10 +3,9 @@
 val lines = java.io.File(args[0]).readLines()
 
 data class Point(val x: Int, val y: Int)
-
-sealed interface Content
-class Symbol(val value: Char) : Content
-class Number(val value: Int) : Content
+object Gear
+object Symbol
+class Number(val value: Int)
 
 val grid =
     buildMap {
@@ -21,7 +20,7 @@ val grid =
                     x += numberString.length
                 } else {
                     if (char != '.') {
-                        put(Point(x, y), Symbol(char))
+                        put(Point(x, y), if (char == '*') Gear else Symbol)
                     }
                     x += 1
                 }
@@ -29,10 +28,10 @@ val grid =
         }
     }
 
-fun getAdjacentContent(p: Point): List<Content> =
+fun getAdjacent(p: Point) =
    (-1..1).flatMap { i -> (-1..1).mapNotNull { j -> grid.get(Point(p.x + i, p.y + j))} }
 
-fun isAdjacentToSymbol(p: Point): Boolean = getAdjacentContent(p).any { it is Symbol}
+fun isAdjacentToSymbol(p: Point): Boolean = getAdjacent(p).any { it is Gear || it is Symbol}
 
 val schematicNumbers =
     grid.entries
@@ -44,8 +43,8 @@ println(schematicNumbers.map { it.value }.sum())
 
 val schematicGearsNumbers = 
     grid.entries
-        .filter { it.value is Symbol && (it.value as Symbol).value == '*' }
-        .map { getAdjacentContent(it.key).filterIsInstance<Number>().toSet() }
+        .filter { it.value is Gear }
+        .map { getAdjacent(it.key).filterIsInstance<Number>().toSet() }
         .filter { it.size == 2 }
         .map { it.toList() }
 
