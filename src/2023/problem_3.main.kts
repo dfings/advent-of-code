13a64445b9131c06a1225d0ff183f387caf50c5b
Dan Fingal-Surma <dfingal@gmail.com>
2023-12-02 22:57:35 -0800

    Update problem_3.main.kts

--- src/2023/problem_3.main.kts
+++ src/2023/problem_3.main.kts
@@ -2,39 +2,53 @@
 
 val lines = java.io.File(args[0]).readLines()
 
-fun Char.isSymbol() = !isDigit() && this != '.'
+data class Point(val x: Int, val y: Int)
 
-fun List<String>.charAt(x: Int, y: Int) = when {
-    y < 0 || y >= size -> '.'
-    x < 0 || x >= get(y).length -> '.'
-    else -> get(y).get(x)
-}
+sealed interface Content
+sealed interface Symbol : Content
+object Gear : Symbol
+object OtherSymbol : Symbol
+class Number(val value: Int) : Content
 
-fun List<String>.isAdjacentToSymbol(x: Int, y: Int): Boolean {
-    for (i in -1..1) {
-        for (j in -1..1) {
-            if (charAt(x + i, y + j).isSymbol()) {
-                return true
+val grid =
+    buildMap {
+        for (y in 0..lines.lastIndex) {
+            var x = 0
+            while (x < lines[y].lastIndex) {
+                val char = lines[y][x]
+                if (char.isDigit()) {
+                    val numberString = lines[y].substring(x).takeWhile { it.isDigit() }
+                    val number = Number(numberString.toInt())
+                    (0..numberString.lastIndex).forEach { i -> put(Point(x + i, y), number) }
+                    x += numberString.length
+                } else {
+                    if (char != '.') {
+                        put(Point(x, y), if (char == '*') Gear else OtherSymbol)
+                    }
+                    x += 1
+                }
             }
         }
     }
-    if (charAt(x + 1, y).isDigit()) {
-        return isAdjacentToSymbol(x + 1, y)
-    }
-    return false
-}
 
-var sum = 0
-for (y in 0..lines.lastIndex) {
-    var x = 0
-    while (x < lines[y].lastIndex) {
-        if (lines.charAt(x, y).isDigit() && lines.isAdjacentToSymbol(x, y)) {
-            val numberString = lines[y].substring(x).takeWhile { it.isDigit() }
-            x += numberString.length
-            sum += numberString.toInt()
-        } else {
-            x += 1
-        }
-    }
-}
-println(sum)
+fun getAdjacentContent(p: Point): List<Content> =
+   (-1..1).flatMap { i -> (-1..1).mapNotNull { j -> grid.get(Point(p.x + i, p.y + j))} }
+
+fun isAdjacentToSymbol(p: Point): Boolean = getAdjacentContent(p).any { it is Symbol}
+
+val schematicNumbers =
+    grid.entries
+        .filter { it.value is Number && isAdjacentToSymbol(it.key) }
+        .map { it.value as Number }
+        .toSet()
+
+println(schematicNumbers.map { it.value }.sum())
+
+val schematicGearsNumbers = 
+    grid.entries
+        .filter { it.value is Gear }
+        .map { getAdjacentContent(it.key).filterIsInstance<Number>().toSet() }
+        .filter { it.size == 2 }
+        .map { it.toList() }
+
+println(schematicGearsNumbers.map { it[0].value * it[1].value }.sum())
