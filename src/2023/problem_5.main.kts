1e7603e04c3fd8e1cd6c852aaff0bc33adeafea5
Dan Fingal-Surma <dfingal@gmail.com>
2023-12-05 10:24:41 -0800

    Update problem_5.main.kts

--- src/2023/problem_5.main.kts
+++ src/2023/problem_5.main.kts
@@ -1,20 +1,24 @@
 #!/usr/bin/env kotlin
 
+import java.util.TreeSet
+
 val lines = java.io.File(args[0]).readLines()
 
-data class AlmanacEntry(val destination: Long, val source: Long, val offset: Long) {
-    operator fun contains(n: Long) = n >= source && n < source + offset
+data class AlmanacEntry(val destination: Long, val source: Long, val length: Long) {
+    operator fun contains(n: Long) = n >= source && n < source + length
 }
 
 fun Iterable<AlmanacEntry>.getDestination(n: Long): Long =
     find { n in it }?.run { n + destination - source } ?: n
 
-fun Iterable<AlmanacEntry>.getSplits(r: LongRange): Iterable<Long> =
-    (
-        listOf(r.start, r.endInclusive + 1) +
-            map { it.source }.filter { it in r } +
-            map { it.source + it.offset }.filter { it in r }
-    ).toSortedSet()
+fun Iterable<AlmanacEntry>.getSplits(r: LongRange): Iterable<Long> = TreeSet<Long>().apply {
+    add(r.start)
+    add(r.endInclusive + 1)
+    this@getSplits.forEach {
+        if (it.source in r) add(it.source)
+        if (it.source + it.length in r) add(it.source + it.length)
+    }
+}
 
 fun Iterable<AlmanacEntry>.getDestinations(r: LongRange): List<LongRange> =
     getSplits(r).windowed(2).map { getDestination(it[0])..getDestination(it[1] - 1) }
@@ -37,4 +41,3 @@ println(locations.min())
 
 val seedRanges = seeds.chunked(2).map { it[0] until it[0] + it[1] }
 val locationRanges = pages.fold(seedRanges) { acc, page -> acc.flatMap { page.getDestinations(it) } }
-println(locationRanges.minOf { it.start })
