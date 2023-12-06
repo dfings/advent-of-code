d435ef312e7c20f7abc6a074ff33f55f76c0b0b1
Dan Fingal-Surma <dfingal@gmail.com>
2023-12-05 21:51:05 -0800

    Update problem_6.main.kts

--- src/2023/problem_6.main.kts
+++ src/2023/problem_6.main.kts
@@ -6,8 +6,7 @@ fun countWinners(time: Long, distance: Long) =
     (1..time).count { velocity -> distance < velocity * (time - velocity) }.toLong()
 
 fun String.parse() = substringAfter(":").trim().split(Regex(" +")).map { it.toLong() }
-val times = lines[0].parse()
-val distances = lines[1].parse()
+val (times, distances) = lines.map { it.parse() }
 println(times.mapIndexed { i, time -> countWinners(time, distances[i]) }.reduce(Long::times))
 
 val bigTime = times.joinToString("").toLong()
