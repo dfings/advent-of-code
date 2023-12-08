f88b32684db1a80fec8cdd37525c8cadeab63a8c
Dan Fingal-Surma <dfingal@gmail.com>
2023-12-08 00:13:31 -0800

    Update problem_8.main.kts

--- src/2023/problem_8.main.kts
+++ src/2023/problem_8.main.kts
@@ -12,12 +12,20 @@ fun String.parse(): Pair<String, List<String>> {
 val directions = lines[0]
 val map = lines.drop(2).map { it.parse() }.toMap()
 
-var node = "AAA"
-var step = 0
-while (node != "ZZZ") {
-    val d = directions[step % directions.length]
-    node = map.getValue(node).get(if (d == 'L') 0 else 1)
-    println(node)
-    step++
+fun pathLength(start: String, done: (String) -> Boolean): Int {
+    var node = start
+    var step = 0
+    while (!done(node)) {
+        val d = directions[step % directions.length]
+        node = map.getValue(node).get(if (d == 'L') 0 else 1)
+        step++
+    }
+    return step
 }
-println(step)
+println(pathLength("AAA") { it == "ZZZ" })
+
+tailrec fun gcd(x: Long, y: Long): Long = if (y == 0L) x else gcd(y, x % y)
+fun lcm(x: Long, y: Long): Long = (x * y) / gcd(x, y)
+
+val lengths = map.keys.filter { it.endsWith('A') }.map { pathLength(it) { it.endsWith('Z') } } 
+println(lengths.fold(1L) { acc, it -> lcm(acc, it.toLong()) })
