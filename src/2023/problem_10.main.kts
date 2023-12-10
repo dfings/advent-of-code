42af7ba914619812ce1d78deb02c0d12eb22d2ff
Dan Fingal-Surma <dfingal@gmail.com>
2023-12-10 02:16:23 -0800

    Update problem_10.main.kts

--- src/2023/problem_10.main.kts
+++ src/2023/problem_10.main.kts
@@ -45,6 +45,7 @@ val projectedPath = path.flatMap {
 
 fun Point.isValidProjected() = x >= -1 && y >= -1 && x <= 2 * (xMax + 1) &&  y <= 2 * (yMax + 1)
 fun Point.neighbors() = Dir.values().map { move(it) }.filter { it.isValidProjected() }
+
 val seen = mutableSetOf(Point(-1, -1))
 val frontier = ArrayDeque<Point>(Point(-1, -1).neighbors().filter { it !in projectedPath })
 while (!frontier.isEmpty()) {
