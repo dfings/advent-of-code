6741bbd7753a861a52079c4a1cad11135cf39b9d
Dan Fingal-Surma <dfingal@gmail.com>
2023-12-10 21:34:13 -0800

    Update problem_11.main.kts

--- src/2023/problem_11.main.kts
+++ src/2023/problem_11.main.kts
@@ -4,22 +4,26 @@ import kotlin.math.abs
 
 val lines = java.io.File(args[0]).readLines()
 
-data class Point(val x: Int, val y: Int)
+data class Point(val x: Long, val y: Long)
 
 val rowsToExpand = lines.indices.filter { y -> '#' !in lines[y] }
 val colsToExpand = lines[0].indices.filter { x -> lines.none { it[x] == '#' } }
 
-fun makePoint(x: Int, y: Int) = 
-    Point(x + colsToExpand.count { x > it }, y + rowsToExpand.count { y > it })
+fun makePoint(x: Int, y: Int, e: Int) = Point(
+    x.toLong() + colsToExpand.count { x > it } * (e -1), 
+    y.toLong() + rowsToExpand.count { y > it } * (e - 1)
+)
 
-val galaxies = lines.flatMapIndexed { y, line ->
-  line.mapIndexedNotNull { x, char -> if (char == '#') makePoint(x, y) else null }
+fun galaxies(e: Int) = lines.flatMapIndexed { y, line ->
+  line.mapIndexedNotNull { x, char -> if (char == '#') makePoint(x, y, e) else null }
 }
 
-fun distance(a: Point, b: Point) = abs(b.x - a.x) + abs(b.y - a.y)
-
 fun allPairs(points: Iterable<Point>): Iterable<Pair<Point, Point>> =
-  points.flatMap { i -> points.mapNotNull { j -> i to j } }
+  points.flatMap { a -> points.mapNotNull { b -> a to b } }
 
-println(allPairs(galaxies).sumOf { (a, b) -> distance(a, b) } / 2)
+fun distance(a: Point, b: Point) = abs(b.x - a.x) + abs(b.y - a.y)
+fun distances(points: Iterable<Point>) = allPairs(points).sumOf { (a, b) -> distance(a, b) } / 2
+
+println(distances(galaxies(2)))
+println(distances(galaxies(1000000)))
 
