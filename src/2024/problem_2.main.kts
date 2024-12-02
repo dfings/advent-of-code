97ca7692adfbd327eff6a404ada2be18ef80584c
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-01 22:07:20 -0800

    Create problem_2.main.kts

---
+++ src/2024/problem_2.main.kts
@@ -0,0 +1,20 @@
+#!/usr/bin/env kotlin
+
+import kotlin.math.abs
+
+val lines = java.io.File(args[0]).readLines()
+val reports = lines.map { it.split(" ").map { it.toInt() } }
+
+// Part 1
+fun Pair<Int, Int>.isSafe() = first < second && second - first in 1..3
+fun List<Int>.isSafe() = zipWithNext().all { it.isSafe() }
+println(reports.count { it.isSafe() || it.reversed().isSafe() })
+
+// Part 2
+fun List<Int>.isSafe2() : Boolean {
+    val badIndex = zipWithNext().indexOfFirst { !it.isSafe() }
+    return badIndex == -1 ||
+        filterIndexed { i, _ -> i != badIndex }.isSafe() ||
+        filterIndexed { i, _ -> i != badIndex + 1 }.isSafe()
+}
+println(reports.count { it.isSafe2() || it.reversed().isSafe2() })
