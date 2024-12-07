20f35245e3d28e9d505423d9323b391e62d17465
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-07 02:56:07 -0800

    Create problem_7.main.kts

---
+++ src/2024/problem_7.main.kts
@@ -0,0 +1,46 @@
+#!/usr/bin/env kotlin
+
+data class Equation(val total: Long, val values: List<Long>)
+fun String.toEquation() = Equation(
+    substringBefore(":").toLong(), 
+    substringAfter(": ").split(" ").map { it.toLong() }
+)
+
+enum class Operator { ADD, TIMES, CONCAT }
+fun Operator.apply(a: Long, b: Long) = when (this) {
+    Operator.ADD -> a + b
+    Operator.TIMES -> a * b
+    Operator.CONCAT -> "$a$b".toLong()
+}
+
+fun Equation.checkRecusive(subtotal: Long, index: Int, op: Operator, validOps: List<Operator>): Boolean {
+    val newSubtotal = op.apply(subtotal, values[index])
+    if (newSubtotal > total) return false
+    if (index == values.lastIndex) return newSubtotal == total
+    return validOps.any { newOp -> checkRecusive(newSubtotal, index + 1, newOp, validOps) }
+}
+
+fun Equation.check(validOps: List<Operator>) =
+    validOps.any { op -> checkRecusive(values[0], 1, op, validOps) }
+
+val lines = java.io.File(args[0]).readLines()
+val equations = lines.map { it.toEquation() }
+println(equations.filter { it.check(Operator.entries.take(2)) }.sumOf { it.total })
+println(equations.filter { it.check(Operator.entries) }.sumOf { it.total })
+
+// This is the first way I solved the problem.
+fun Equation.checkOriginal(): Boolean {
+    val combos = (2 shl values.size) - 1
+    for (i in 0..combos) {
+        var result = values[0]
+        for (j in 1..values.lastIndex) {
+            when ((i shr j) and 1) {
+                0 -> result += values[j]
+                1 -> result *= values[j]
+            }
+            if (result > total) break
+        }
+        if (total == result) return true
+    }
+    return false
+}
