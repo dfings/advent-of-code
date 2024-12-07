efbc9ebb19f7b3fd168a66ee5a50752e7b5bf779
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-07 12:22:25 -0800

    Update problem_7.main.kts

--- src/2024/problem_7.main.kts
+++ src/2024/problem_7.main.kts
@@ -15,9 +15,11 @@ operator fun Operator.invoke(a: Long, b: Long) = when (this) {
 
 fun Equation.checkRecusive(subtotal: Long, index: Int, op: Operator, validOps: List<Operator>): Boolean {
     val newSubtotal = op(subtotal, values[index])
-    if (newSubtotal > total) return false
-    if (index == values.lastIndex) return newSubtotal == total
-    return validOps.any { newOp -> checkRecusive(newSubtotal, index + 1, newOp, validOps) }
+    return when {
+       newSubtotal > total -> false
+       index == values.lastIndex -> newSubtotal == total
+       else -> validOps.any { newOp -> checkRecusive(newSubtotal, index + 1, newOp, validOps) }
+    }
 }
 
 fun Equation.check(validOps: List<Operator>) =
