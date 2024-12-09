3866c604d6a30335b4475ab36d3b06805371d8f2
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-08 20:06:29 -0800

    Update problem_8.main.kts

--- src/2024/problem_8.main.kts
+++ src/2024/problem_8.main.kts
@@ -1,12 +1,9 @@
 #!/usr/bin/env kotlin
 
-data class Point(val x: Int, val y: Int)
-data class Vector(val dx: Int, val dy: Int)
-fun Vector(a: Point, b: Point) = Vector(b.x  - a.x, b.y - a.y)
-
 fun <T> Iterable<T>.allPairs() =
     flatMap { i -> mapNotNull { j -> if(i == j) null else i to j }}
 
+data class Point(val x: Int, val y: Int)
 class Antenna(val grid: List<String>) {
     val xRange = 0..grid[0].lastIndex
     val yRange = 0..grid.lastIndex
@@ -17,8 +14,9 @@ class Antenna(val grid: List<String>) {
     operator fun contains(p: Point) = p.x in xRange && p.y in yRange
 
     fun generateAntinodes(a: Point, b: Point): Sequence<Point> {
-        val v = Vector(a, b)
-        return generateSequence(b) { Point(it.x + v.dx, it.y + v.dy) }.takeWhile { it in this }
+        val dx = b.x - a.x
+        val dy = b.y - a.y
+        return generateSequence(b) { Point(it.x + dx, it.y + dy) }.takeWhile { it in this }
     }
 
     fun pairwiseAntinodes(points: Iterable<Point>, select: (Sequence<Point>) -> Sequence<Point>) = 
