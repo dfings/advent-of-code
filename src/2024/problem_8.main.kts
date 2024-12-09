18f73cd85398b48009dc39c9841998be247d089a
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-08 19:55:38 -0800

    Create problem_8.main.kts

---
+++ src/2024/problem_8.main.kts
@@ -0,0 +1,34 @@
+#!/usr/bin/env kotlin
+
+data class Point(val x: Int, val y: Int)
+data class Vector(val dx: Int, val dy: Int)
+fun Vector(a: Point, b: Point) = Vector(b.x  - a.x, b.y - a.y)
+
+fun <T> Iterable<T>.allPairs() =
+    flatMap { i -> mapNotNull { j -> if(i == j) null else i to j }}
+
+class Antenna(val grid: List<String>) {
+    val xRange = 0..grid[0].lastIndex
+    val yRange = 0..grid.lastIndex
+    val pointMap = xRange.flatMap { x -> yRange.map { y -> grid[y][x] to Point(x, y) } }
+        .filter { it.first != '.' }
+        .groupBy( { it.first }, { it.second })
+
+    fun isInRange(p: Point) = p.x in xRange && p.y in yRange
+
+    fun generateAntinodes(a: Point, b: Point): Sequence<Point> {
+        val v = Vector(a, b)
+        return generateSequence(b) { Point(it.x + v.dx, it.y + v.dy) }.takeWhile(this::isInRange)
+    }
+
+    fun pairwiseAntinodes(points: Collection<Point>, select: (Sequence<Point>) -> Sequence<Point>) = 
+        points.allPairs().flatMap { (a, b) -> select(generateAntinodes(a, b)) }
+
+    fun countAntinodes(select: (Sequence<Point>) -> Sequence<Point>) = 
+        pointMap.values.flatMap { pairwiseAntinodes(it, select) }.toSet().size
+}
+
+val lines = java.io.File(args[0]).readLines()
+val antenna = Antenna(lines)
+println(antenna.countAntinodes { it.drop(1).take(1) })
+println(antenna.countAntinodes { it })
