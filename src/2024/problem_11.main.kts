c4949d489f237679d3e68481c1bd5c202f5549a7
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-11 00:38:01 -0800

    Update problem_11.main.kts

--- src/2024/problem_11.main.kts
+++ src/2024/problem_11.main.kts
@@ -16,3 +16,15 @@ val line = java.io.File(args[0]).readLines().single()
 val stones = line.split(" ").map { it.toLong() }
 println(stones.sumOf { countStones(it, 25) })
 println(stones.sumOf { countStones(it, 75) })
+
+// Alternate solution
+fun Map<Long, Long>.blink(): Map<Long, Long> =
+    entries.flatMap { (stone, count) -> blink(stone).map { it to count } }
+        .groupBy({ it.first }, { it.second })
+        .mapValues { (k, v) -> v.sum() }
+
+var stoneCounts = stones.groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+repeat (25) { stoneCounts = stoneCounts.blink() }
+println(stoneCounts.values.sum())
+repeat (50) { stoneCounts = stoneCounts.blink() }
+println(stoneCounts.values.sum())
