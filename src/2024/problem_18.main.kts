9d89651ad53af2b21caacafac554aa33b6b2e265
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-17 21:29:54 -0800

    Create problem_18.main.kts

---
+++ src/2024/problem_18.main.kts
@@ -0,0 +1,51 @@
+#!/usr/bin/env kotlin
+
+enum class Direction(val x: Int, val y: Int) {
+    NORTH(0, -1), EAST(1, 0), SOUTH(0, 1), WEST(-1, 0)
+}
+
+data class Point(val x: Int, val y: Int)
+operator fun Point.plus(d: Direction) = Point(x + d.x, y + d.y)
+
+data class Grid(val lastIndex: Int, val corrupted: Set<Point>) {
+    val range = 0..lastIndex
+    fun neighbors(p: Point) = Direction.entries.map { p + it }
+        .filter { it.x in range && it.y in range && it !in corrupted }
+
+    fun findShortestPathLength(): Int {
+        val start = Point(0, 0)
+        val end = Point(lastIndex, lastIndex)
+        val minCost = mutableMapOf(start to 0)
+        val frontier = mutableSetOf(start to 0)
+        while (!frontier.isEmpty()) {
+            val (point, cost) = frontier.minBy { it.second }
+            frontier.remove(point to cost)
+            if (point == end) return cost
+            for (n in neighbors(point)) {
+                val oldCost = minCost[n] ?: Int.MAX_VALUE
+                val newCost = cost + 1
+                if (newCost < oldCost) {  
+                    minCost[n] = newCost
+                    frontier.add(n to newCost)
+                }
+            }
+        }
+        return -1
+    }
+}
+
+val lines = java.io.File(args[0]).readLines()
+val corrupted = lines.map { it.split(",").let { Point(it[0].toInt(), it[1].toInt()) } }
+
+val SIZE = 70
+val memory1 = Grid(SIZE, corrupted.take(1024).toSet())
+println(memory1.findShortestPathLength())
+
+for (i in 1..corrupted.size) {
+    val memory2 = Grid(SIZE, corrupted.take(i).toSet())
+    if (memory2.findShortestPathLength() == -1) {
+        val c = corrupted[i - 1]
+        println("${c.x},${c.y}")
+        break
+    }
+}
