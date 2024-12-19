b7b5b16f9ee6c8681068369bc9410d438259d758
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-19 00:56:03 -0800

    Update problem_19.main.kts

--- src/2024/problem_19.main.kts
+++ src/2024/problem_19.main.kts
@@ -5,13 +5,13 @@ val towels = lines[0].split(", ")
 val patterns = lines.drop(2)
 
 val cache = mutableMapOf<String, Long>()
-fun countSequences(pattern: String): Long = when {
-    pattern.isEmpty() -> 1
-    else -> cache.getOrPut(pattern) {
-        towels.filter { pattern.startsWith(it) }.sumOf { countSequences(pattern.removePrefix(it)) }
+fun String.countSequences(): Long = when {
+    isEmpty() -> 1
+    else -> cache.getOrPut(this) {
+        towels.filter { startsWith(it) }.sumOf { removePrefix(it).countSequences() }
     }
 }
 
-val sequenceCounts = patterns.map { countSequences(it) }
+val sequenceCounts = patterns.map { it.countSequences() }
 println(sequenceCounts.count {  it > 0 })
 println(sequenceCounts.sum())
