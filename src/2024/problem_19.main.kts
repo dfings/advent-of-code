d59ac604f799f7b356ac013a39a60a557191ff40
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-18 23:23:29 -0800

    Update problem_19.main.kts

--- src/2024/problem_19.main.kts
+++ src/2024/problem_19.main.kts
@@ -5,14 +5,13 @@ val towels = lines[0].split(", ")
 val patterns = lines.drop(2)
 
 val cache = mutableMapOf<String, Long>()
-fun countTowelSequences(pattern: String, partial: String): Long = when {
-    partial == pattern -> 1L
-    !pattern.startsWith(partial) -> 0L
-    else -> cache.getOrPut(pattern.drop(partial.length)) {
-        towels.sumOf { countTowelSequences(pattern, partial + it) }
+fun countTowelSequences(pattern: String): Long = when {
+    pattern.isEmpty() -> 1
+    else -> cache.getOrPut(pattern) {
+        towels.filter { pattern.startsWith(it) }.sumOf { countTowelSequences(pattern.drop(it.length)) }
     }
 }
 
-val sequenceCounts = patterns.map { countTowelSequences(it, "") }
+val sequenceCounts = patterns.map { countTowelSequences(it) }
 println(sequenceCounts.count {  it > 0 })
 println(sequenceCounts.sum())
