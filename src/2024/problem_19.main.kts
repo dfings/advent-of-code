f2220927c032871736cb0567665a13dd929e3307
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-19 01:28:46 -0800

    Update problem_19.main.kts

--- src/2024/problem_19.main.kts
+++ src/2024/problem_19.main.kts
@@ -1,12 +1,12 @@
 #!/usr/bin/env kotlin
 
 val lines = java.io.File(args[0]).readLines()
-val (towels, patterns) = lines[0].split(", ") to lines.drop(2)
+val (patterns, designs) = lines[0].split(", ") to lines.drop(2)
 
 val cache = mutableMapOf<String, Long>("" to 1)
 fun String.countSequences(): Long = cache.getOrPut(this) {
-    towels.filter { startsWith(it) }.sumOf { removePrefix(it).countSequences() }
+    patterns.filter { startsWith(it) }.sumOf { removePrefix(it).countSequences() }
 }
 
-println(patterns.count { it.countSequences() > 0 })
-println(patterns.sumOf { it.countSequences() })
+println(designs.count { it.countSequences() > 0 })
+println(designs.sumOf { it.countSequences() })
