5651a38b98d1e1ded4e95ea95f14ec5a8c48c83f
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-22 01:30:22 -0800

    Update problem_22.main.kts

--- src/2024/problem_22.main.kts
+++ src/2024/problem_22.main.kts
@@ -1,5 +1,7 @@
 #!/usr/bin/env kotlin
 
+import kotlin.time.measureTime
+
 fun mixAndPrune(v: Long, s: Long) = (v xor s) % (1 shl 24)
 fun next(s: Long): Long {
     val a = mixAndPrune(s shl 6, s)
@@ -9,11 +11,11 @@ fun next(s: Long): Long {
 
 fun secretNumbers(s: Long) = generateSequence(s, ::next)
 
-fun getPriceSequenceMap(s: Long): Map<List<Int>, Int> {
+fun getPriceSequenceMap(s: Long): Map<String, Int> {
     val prices = secretNumbers(s).map { (it % 10).toInt() }.take(2000)
     val changes = prices.zipWithNext().map { (a, b) -> b - a }.windowed(4)
-    val output = mutableMapOf<List<Int>, Int>()
-    changes.zip(prices.drop(4)).forEach { (k, v) -> output.putIfAbsent(k, v) }
+    val output = mutableMapOf<String, Int>()
+    changes.zip(prices.drop(4)).forEach { (k, v) -> output.putIfAbsent("$k", v) }
     return output
 }
 
@@ -23,3 +25,8 @@ println(initial.sumOf { secretNumbers(it).drop(2000).take(1).single() })
 val histograms = initial.map { getPriceSequenceMap(it) }
 val candidates = histograms.asSequence().flatMap { it.keys }.toSet()
 println(candidates.maxOf { c -> histograms.sumOf { it[c] ?: 0 } })
+
+val t = measureTime {
+    candidates.maxOf { c -> histograms.sumOf { it[c] ?: 0 } }
+}
+println(t)
