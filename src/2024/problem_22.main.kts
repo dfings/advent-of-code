73a0e0c11622d04e074620a7746ecd1101fb2604
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-22 03:32:32 -0800

    Update problem_22.main.kts

--- src/2024/problem_22.main.kts
+++ src/2024/problem_22.main.kts
@@ -1,5 +1,7 @@
 #!/usr/bin/env kotlin
 
+import kotlin.time.measureTime
+
 fun mixAndPrune(v: Long, s: Long) = (v xor s) % (1 shl 24)
 fun next(s: Long): Long {
     val a = mixAndPrune(s shl 6, s)
@@ -9,20 +11,24 @@ fun next(s: Long): Long {
 
 fun secretNumbers(s: Long) = generateSequence(s, ::next)
 
-fun MutableMap<String, Int>.addPriceSequences(s: Long) {
+
+fun IntArray.addPriceSequences(s: Long) {
     val prices = secretNumbers(s).map { (it % 10).toInt() }.take(2000)
     val changes = prices.zipWithNext()
-        .map { (a, b) -> b - a }
+        .map { (a, b) -> (b - a).mod(19) }
         .windowed(4)
-        .map { (a, b, c, d) -> "$a $b $c $d" }
+        .map { (a, b, c, d) -> a * 6859 + b * 361 + c * 19 + d }
     for ((k, v) in changes.zip(prices.drop(4)).distinctBy { it.first }) {
-        put(k, v + (get(k) ?: 0))
+        this[k] += v
     }
 }
 
 val initialValues = java.io.File(args[0]).readLines().map { it.toLong() }
 println(initialValues.sumOf { secretNumbers(it).drop(2000).take(1).single() })
 
-val sequencePrices = HashMap<String, Int>()
+val t = measureTime {
+val sequencePrices = IntArray(19 * 19 * 19 * 19) { 0 }
 initialValues.map { sequencePrices.addPriceSequences(it) }
-println(sequencePrices.values.max())
+println(sequencePrices.max())
+}
+println(t)
