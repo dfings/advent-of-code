1fe5b991223d56f9638428fe283132550daa8dbc
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-22 01:58:59 -0800

    Update problem_22.main.kts

--- src/2024/problem_22.main.kts
+++ src/2024/problem_22.main.kts
@@ -14,7 +14,7 @@ fun secretNumbers(s: Long) = generateSequence(s, ::next)
 fun getPriceSequenceMap(s: Long): Map<String, Int> {
     val prices = secretNumbers(s).map { (it % 10).toInt() }.take(2000)
     val changes = prices.zipWithNext().map { (a, b) -> b - a }.windowed(4)
-    val output = mutableMapOf<String, Int>()
+    val output = HashMap<String, Int>()
     changes.zip(prices.drop(4)).forEach { (k, v) -> output.putIfAbsent("$k", v) }
     return output
 }
@@ -22,11 +22,9 @@ fun getPriceSequenceMap(s: Long): Map<String, Int> {
 val initial = java.io.File(args[0]).readLines().map { it.toLong() }
 println(initial.sumOf { secretNumbers(it).drop(2000).take(1).single() })
 
+val t = measureTime {
 val histograms = initial.map { getPriceSequenceMap(it) }
 val candidates = histograms.asSequence().flatMap { it.keys }.toSet()
 println(candidates.maxOf { c -> histograms.sumOf { it[c] ?: 0 } })
-
-val t = measureTime {
-    candidates.maxOf { c -> histograms.sumOf { it[c] ?: 0 } }
 }
 println(t)
