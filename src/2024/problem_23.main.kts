ea0967b3e9dd774f433bf0c994cb2fc6275cbe09
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-23 13:02:24 -0800

    Update problem_23.main.kts

--- src/2024/problem_23.main.kts
+++ src/2024/problem_23.main.kts
@@ -1,27 +1,29 @@
 #!/usr/bin/env kotlin
 
 import java.util.BitSet
+
 fun String.encode() = (this[0] - 'a') * 26 + (this[1] - 'a')
 fun Int.decode() = "${'a' + (this / 26)}${'a' + (this % 26)}"
 
 fun makeBitSet(bitIndexes: Iterable<Int> = emptyList()) = 
     BitSet(26 * 26).apply { bitIndexes.forEach { set(it) } }
 
-fun BitSet.bitIndexes() = stream().toArray()
-fun BitSet.copy() = clone() as BitSet
-fun BitSet.intersect(other: BitSet) = copy().apply { and(other) } 
-fun BitSet.union(other: BitSet) = copy().apply { or(other) } 
-operator fun BitSet.plus(index: Int) = copy().apply { set(index) }
-operator fun BitSet.minus(index: Int) = copy().apply { set(index, false) }
+fun BitSet.bitIndexes(): IntArray = stream().toArray()
+fun BitSet.intersect(other: BitSet) = (clone() as BitSet).apply { and(other) } 
+operator fun BitSet.plus(index: Int) = (clone() as BitSet).apply { set(index) }
 
 val lines = java.io.File(args[0]).readLines()
 val pairs = lines.map { makeBitSet(it.split("-").map { it.encode() }) }.toSet()
 
-val links = pairs.flatMap { clique -> clique.bitIndexes().map { it to (clique - it) } }
-    .groupingBy { it.first }
-    .fold(makeBitSet()) { acc, it -> acc.union(it.second) }
+val links = Array(26 * 26) { makeBitSet() }
+for (pair in pairs) {
+    for (bitIndex in pair.bitIndexes()) {
+        links[bitIndex].or(pair)
+        links[bitIndex].clear(bitIndex)
+    }
+}
 
-fun BitSet.intersectLinks() = bitIndexes().map { links.getValue(it) }.reduce(BitSet::intersect)
+fun BitSet.intersectLinks() = bitIndexes().map { links[it] }.reduce(BitSet::intersect)
 fun Set<BitSet>.next() = flatMap { clique ->
     clique.intersectLinks().bitIndexes().map { clique + it }
 }.toSet()
