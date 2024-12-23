bc94ba05d2df98265668210d24dfa1ea2fa395e7
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-23 00:50:48 -0800

    Update problem_23.main.kts

--- src/2024/problem_23.main.kts
+++ src/2024/problem_23.main.kts
@@ -1,23 +1,15 @@
 #!/usr/bin/env kotlin
 
 val lines = java.io.File(args[0]).readLines()
-val links = lines.map { it.split("-") }
+val pairs = lines.map { it.split("-").toSet() }.toSet()
 
-val connections = (links + links.map { it.reversed() }).groupBy({ it[0] }, { it[1] }).mapValues { it.value.toSet() }
-var triples = mutableSetOf<Set<String>>()
-for (first in connections.keys) {
-    for (second in connections.getValue(first)) {
-        for (third in connections.getValue(second)) {
-            if (first in connections.getValue(third)) {
-                triples.add(setOf(first, second, third))
-            }
-        }
-    }
+val links = mutableMapOf<String, MutableSet<String>>()
+for (pair in pairs) {
+    links.getOrPut(pair.first()) { HashSet<String>() }.add(pair.last())
+    links.getOrPut(pair.last()) { HashSet<String>() }.add(pair.first())
 }
-println(triples.count { it.any { it.startsWith("t") } })
 
-fun Set<String>.fullyConnected() = map { connections.getValue(it) }.reduce(Set<String>::intersect)
-
+fun Set<String>.fullyConnected() = map { links.getValue(it) }.reduce(Set<String>::intersect)
 fun Set<Set<String>>.next(): Set<Set<String>> {
     val n = mutableSetOf<Set<String>>()
     for (s in this) {
@@ -28,14 +20,11 @@ fun Set<Set<String>>.next(): Set<Set<String>> {
     return n
 }
 
-val quads = triples.next()
-val fives = quads.next()
-val sixes = fives.next()
-val sevens = sixes.next()
-val eights = sevens.next()
-val nines = eights.next()
-val tens = nines.next()
-val elevens = tens.next()
-val twelves = elevens.next()
-val thirteens = twelves.next()
-println(thirteens.single().sorted().joinToString(","))
+var triples = pairs.next()
+println(triples.count { it.any { it.startsWith("t") } })
+
+var maximal = triples
+while (maximal.size > 1) {
+    maximal = maximal.next()
+}
+println(maximal.single().sorted().joinToString(","))
