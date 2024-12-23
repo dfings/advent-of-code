8e1d5e4209c893145a5138efd6aa79ac41b2ae5a
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-23 01:52:54 -0800

    Update problem_23.main.kts

--- src/2024/problem_23.main.kts
+++ src/2024/problem_23.main.kts
@@ -20,3 +20,31 @@ while (maximalCliques.size > 1) {
     maximalCliques = maximalCliques.next()
 }
 println(maximalCliques.single().sorted().joinToString(","))
+
+class BronKerboschSolver<V>(private val graph: Map<V, Set<V>>) {
+    private var result = emptyList<V>()
+
+    fun solve(): List<V> {
+        bronKerbosch(emptyList(), graph.keys.toList(), emptyList())
+        return result
+    }
+
+    private fun bronKerbosch(r: List<V>, p: List<V>, x: List<V>) {
+        if (p.isEmpty() && x.isEmpty()) {
+            if (result.size < r.size) result = r
+            return
+        }
+        val pivot = p.firstOrNull() ?: x.first()
+        val p1 = p.toMutableList()
+        val x1 = x.toMutableList()
+        for (v in (p - graph.getValue(pivot))) {
+            val es = graph.getValue(v)
+            bronKerbosch(r + v, p1.filter { it in es }, x1.filter { it in es })
+            p1 -= v
+            x1 += v
+        }
+    }
+}
+
+val solver = BronKerboschSolver(links)
+println(solver.solve().sorted().joinToString(","))
