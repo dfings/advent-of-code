a67b5c6beb2deec9cbe6509ff38488f785187074
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-23 00:29:31 -0800

    Update problem_23.main.kts

--- src/2024/problem_23.main.kts
+++ src/2024/problem_23.main.kts
@@ -3,9 +3,9 @@
 val lines = java.io.File(args[0]).readLines()
 val links = lines.map { it.split("-") }
 
-val connections = (links + links.map { it.reversed() }).groupBy({ it[0] }, { it[1] })
+val connections = (links + links.map { it.reversed() }).groupBy({ it[0] }, { it[1] }).mapValues { it.value.toSet() }
 var triples = mutableSetOf<Set<String>>()
-for (first in connections.keys.filter { it.startsWith("t") }) {
+for (first in connections.keys) {
     for (second in connections.getValue(first)) {
         for (third in connections.getValue(second)) {
             if (first in connections.getValue(third)) {
@@ -14,5 +14,28 @@ for (first in connections.keys.filter { it.startsWith("t") }) {
         }
     }
 }
-println(triples.size)
+println(triples.count { it.any { it.startsWith("t") } })
 
+fun Set<String>.fullyConnected() = map { connections.getValue(it) }.reduce(Set<String>::intersect)
+
+fun Set<Set<String>>.next(): Set<Set<String>> {
+    val n = mutableSetOf<Set<String>>()
+    for (s in this) {
+        for (candidate in s.fullyConnected()) {
+            n.add(s + candidate)
+        }
+    }
+    return n
+}
+
+val quads = triples.next()
+val fives = quads.next()
+val sixes = fives.next()
+val sevens = sixes.next()
+val eights = sevens.next()
+val nines = eights.next()
+val tens = nines.next()
+val elevens = tens.next()
+val twelves = elevens.next()
+val thirteens = twelves.next()
+println(thirteens.single().sorted().joinToString(","))
