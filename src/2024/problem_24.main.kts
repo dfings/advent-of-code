97534854d2bec6c433aa7dd2f0833bf5714a43fa
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-24 15:56:23 -0800

    Update problem_24.main.kts

--- src/2024/problem_24.main.kts
+++ src/2024/problem_24.main.kts
@@ -4,50 +4,76 @@ sealed interface Gate {
     val value: Boolean
 }
 
-data class Wire(val name: String, var input: Gate) {}
+data class Wire(val name: String, var input: Gate) {
+    val value: Boolean
+        get() = input.value
+}
 
 data class InputGate(override var value: Boolean) : Gate
 
-data class AndGate(var a: Wire, var b: Wire) : Gate {
-    override val value: Boolean
-        get() = a.input.value && b.input.value
+interface BinaryGate : Gate {
+    val input1: Wire
+    val input2: Wire
+    val key: Set<String>
+        get() = setOf(input1.name, input2.name)
 }
 
-data class OrGate(var a: Wire, var b: Wire) : Gate {
+data class AndGate(override val input1: Wire, override val input2: Wire) : BinaryGate {
     override val value: Boolean
-        get() = a.input.value || b.input.value
+        get() = input1.value && input2.value
 }
 
-data class XorGate(var a: Wire, var b: Wire) : Gate {
+data class OrGate(override val input1: Wire, override val input2: Wire) : BinaryGate {
     override val value: Boolean
-        get() = a.input.value xor b.input.value
+        get() = input1.value || input2.value
+}
+
+data class XorGate(override val input1: Wire, override val input2: Wire) : BinaryGate {
+    override val value: Boolean
+        get() = input1.value xor input2.value
 }
 
 val logicPattern = Regex("""(\w+) (\w+) (\w+) -> (\w+)""")
 
 class Device {
-    val wires = mutableMapOf<String, Wire>()
+    val wires = java.util.TreeMap<String, Wire>()
     lateinit var initialLogic: Map<String, String>
     lateinit var xInput: List<InputGate>
     lateinit var yInput: List<InputGate>
     lateinit var zWires: List<Wire>
+    lateinit var andOutput: Map<Set<String>, Wire>
+    lateinit var orOutput: Map<Set<String>, Wire>
+    lateinit var xorOutput: Map<Set<String>, Wire>
+    lateinit var finalCarry: Array<String>
 
     fun loadInput(input: List<String>) {
         for (definition in input) {
             val (name, value) = definition.split(": ")
             wires[name] = Wire(name, InputGate(value == "1"))
         }
-        val (x, y) = wires.entries.sortedBy { it.key }.partition { it.key[0] == 'x' }
-        xInput = x.map { it.value.input as InputGate }
-        yInput = y.map { it.value.input as InputGate }
+        xInput = wires.entries.filter { it.key[0] == 'x' }.map { it.value.input as InputGate }
+        yInput = wires.entries.filter { it.key[0] == 'y' }.map { it.value.input as InputGate }
     }
 
     fun loadLogic(input: List<String>) {
-        initialLogic = input.map { it.takeLast(3) to it }.toMap()
+        initialLogic = input.associateBy { it.takeLast(3) }
         for ((name, definition) in initialLogic.entries) {
             addLogic(name, definition)
         }
-        zWires = wires.entries.filter { it.key[0] == 'z' }.sortedBy { it.key }.map { it.value }
+        zWires = wires.entries.filter { it.key[0] == 'y' }.map { it.value }
+        finalCarry = Array(zWires.size) { "" }
+        reindex()
+    }
+
+    private fun reindex() {
+        val gates = wires.values.map { it.input }
+        val wiresByInputGate = java.util.IdentityHashMap<Gate, Wire>()
+        wires.values.associateByTo(wiresByInputGate) { it.input }
+        fun <T : BinaryGate> List<T>.makeWireMap() =
+            associateBy { it.key }.mapValues { wiresByInputGate.getValue(it.value) }
+        andOutput = gates.filterIsInstance<AndGate>().makeWireMap()
+        orOutput = gates.filterIsInstance<OrGate>().makeWireMap()
+        xorOutput = gates.filterIsInstance<XorGate>().makeWireMap()
     }
 
     private fun addLogic(name: String, definition: String): Wire {
@@ -63,6 +89,39 @@ class Device {
         }
         wires[name] = newWire
         return newWire
+    }
+
+    fun fixErrors() {
+        for (i in 0..zWires.lastIndex - 1) {
+            analyze(i)
+        }
+    }
+
+    private fun analyze(bitIndex: Int) {
+        if (bitIndex == 0) {
+            if (xorOutput.getValue(setOf("x00", "y00")).name != "z00") {
+                throw IllegalArgumentException("Swap z00 into x00 XOR y00")
+            }
+            println("x00 XOR y00 -> z00")
+            finalCarry[0] = andOutput.getValue(setOf("x00", "y00")).name
+            println("x00 AND y00 -> ${finalCarry[0]}")
+        } else {
+            val i = if (bitIndex < 10) "0$bitIndex" else "$bitIndex"
+            val previousFinalCarry = finalCarry[bitIndex - 1]
+            val firstResult = xorOutput.getValue(setOf("x$i", "y$i")).name
+            println("x$i XOR y$i -> $firstResult")
+            val firstCarry = andOutput.getValue(setOf("x$i", "y$i")).name
+            println("x$i AND y$i -> $firstCarry")
+            if (xorOutput.getValue(setOf(firstResult, previousFinalCarry)).name != "z$i") {
+                throw IllegalArgumentException("Swap z$i into x$i XOR y$i")
+            }
+            println("$firstResult XOR $previousFinalCarry -> z$i")
+            val secondCarry = andOutput.getValue(setOf("$firstResult", "$previousFinalCarry")).name
+            println("$firstResult AND $previousFinalCarry -> $secondCarry")
+            finalCarry[bitIndex] = orOutput.getValue(setOf("$firstCarry", "$secondCarry")).name
+            println("$firstCarry OR $secondCarry -> ${finalCarry[bitIndex]}")
+        }
+        println()
     }
 
     fun setInput(x: Long, y: Long) {
@@ -86,43 +145,6 @@ class Device {
     }
 }
 
-data class Rule(val input: Set<String>, val op: String, val output: String)
-class RuleSet(logic: List<String>, val numBits: Int) {
-    val rules = logic.map { 
-        val (a, op, b, c) = logicPattern.find(it)!!.destructured
-        Rule(setOf(a, b), op, c)
-    }
-    val rulesMap = rules.map { (it.input to it.op) to it }.toMap()
-    val finalCarry = Array(numBits) { "" }
-
-    fun analyze(bitIndex: Int) {
-        if (bitIndex == 0) {
-            if (rulesMap.getValue(setOf("x00", "y00") to "XOR").output != "z00") {
-                throw IllegalArgumentException("Swap z00 into x00 XOR y00")
-            }
-            println("x00 XOR y00 -> z00")
-            finalCarry[0] = rulesMap.getValue(setOf("x00", "y00") to "AND").output
-            println("x00 AND y00 -> ${finalCarry[0]}")
-        } else {
-            val i = if (bitIndex < 10) "0$bitIndex" else "$bitIndex"
-            val previousFinalCarry = finalCarry[bitIndex - 1]
-            val firstResult = rulesMap.getValue(setOf("x$i", "y$i") to "XOR").output
-            println("x$i XOR y$i -> $firstResult")
-            val firstCarry = rulesMap.getValue(setOf("x$i", "y$i") to "AND").output
-            println("x$i AND y$i -> $firstCarry")
-            if (rulesMap.getValue(setOf(firstResult, previousFinalCarry) to "XOR").output != "z$i") {
-                throw IllegalArgumentException("Swap z$i into $firstResult XOR $previousFinalCarry")
-            }
-            println("$firstResult XOR $previousFinalCarry -> z$i")
-            val secondCarry = rulesMap.getValue(setOf("$firstResult", "$previousFinalCarry") to "AND").output
-            println("$firstResult AND $previousFinalCarry -> $secondCarry")
-            finalCarry[bitIndex] = rulesMap.getValue(setOf("$firstCarry", "$secondCarry") to "OR").output
-            println("$firstCarry OR $secondCarry -> ${finalCarry[bitIndex]}")
-        }
-        println()
-    }
-}
-
 val lines = java.io.File(args[0]).readLines()
 val logicLines = lines.dropWhile { it != "" }.drop(1)
 
@@ -131,10 +153,4 @@ device.loadInput(lines.takeWhile { it != "" })
 device.loadLogic(logicLines)
 
 println(device.getOutput())
-
-// Right now this throws an exception if it detects an inconsistency
-// in the addition logic, which instructs you to change the input to continue.
-val ruleSet = RuleSet(logicLines, device.xInput.size)
-for (i in 0..ruleSet.numBits - 1) {
-    ruleSet.analyze(i)
-}
+device.fixErrors()
