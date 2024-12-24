5a13da09343858336b73bb10e797df0344793646
Dan Fingal-Surma <dfingal@gmail.com>
2024-12-23 23:22:49 -0800

    Update problem_24.main.kts

--- src/2024/problem_24.main.kts
+++ src/2024/problem_24.main.kts
@@ -6,7 +6,7 @@ sealed interface Gate {
 
 data class Wire(val name: String, var input: Gate) {}
 
-data class InputGate(override val value: Boolean) : Gate
+data class InputGate(override var value: Boolean) : Gate
 
 data class AndGate(var a: Wire, var b: Wire) : Gate {
     override val value: Boolean
@@ -22,6 +22,22 @@ data class XorGate(var a: Wire, var b: Wire) : Gate {
     override val value: Boolean
         get() = a.input.value xor b.input.value
 }
+
+fun Map<String, Wire>.getSortedWires(wirePrefix: String): List<Wire> =
+    entries.filter { it.key.startsWith(wirePrefix) }
+        .sortedBy { it.key }.map { it.value }
+
+fun Map<String, Wire>.setLong(wirePrefix: String, value: Long) {
+    val input = getSortedWires(wirePrefix).map { it.input as InputGate }
+    for ((i, gate) in input.withIndex()) {
+        gate.value = (value and (1L shl i)) != 0L
+    }
+}
+
+fun Map<String, Wire>.toLong(wirePrefix: String) = 
+    getSortedWires(wirePrefix)
+        .mapIndexed { i, it -> if (it.input.value) 1L shl i else 0L }
+        .reduce { acc, it -> acc or it }
 
 val lines = java.io.File(args[0]).readLines()
 val wires = mutableMapOf<String, Wire>()
@@ -51,8 +67,4 @@ for ((name, definition) in logic.entries) {
     addLogicWireAndGate(name, definition)
 }
 
-val zWires = wires.entries.filter { it.key.startsWith("z") }.sortedBy { it.key }
-val part1 = zWires.map { it.value.input.value }
-    .mapIndexed { i, it -> if (it) 1L shl i else 0L }
-    .reduce { acc, it -> acc or it }
-println(part1)
+println(wires.toLong("z"))
