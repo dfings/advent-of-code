b82236e00c963b5842c7f7ee9e1788aefdb1ddf0
Dan Fingal-Surma <dfingal@gmail.com>
2025-01-11 02:26:05 -0800

    Update problem_17.main.kts

--- src/2015/problem_17.main.kts
+++ src/2015/problem_17.main.kts
@@ -5,23 +5,21 @@ class Jug(val liters: Int)
 val lines = java.io.File(args[0]).readLines()
 val jugs = lines.map { Jug(it.toInt()) }
 
-val cache = mutableMapOf<Pair<List<Jug>, Int>, Int>()
-fun fitCheck(
-    used: List<Jug>, 
-    unused: List<Jug>, 
-    remaining: Int
-): Int = cache.getOrPut(used to remaining) {
-    if (remaining == 0) return@getOrPut 1 
+val combos = mutableListOf<List<Jug>>()
+fun fitCheck(used: List<Jug>, unused: List<Jug>, remaining: Int): Int {
+    if (remaining == 0) {
+        combos.add(used)
+        return 1 
+    }
     var total = 0
     for ((i, jug) in unused.withIndex()) {
         if (jug.liters <= remaining) {
             total += fitCheck(used + jug, unused.drop(i + 1), remaining - jug.liters)
         }
     }
-    return@getOrPut total
+    return total
 }
 
 println(fitCheck(emptyList(), jugs, 150))
-val combos = cache.keys.filter { it.second == 0 }.map { it.first }
 val minJugs = combos.minOf { it.size }
 println(combos.count { it.size == minJugs })
