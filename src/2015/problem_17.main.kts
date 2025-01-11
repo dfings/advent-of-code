4f6214e0617dea5352326101dfb17738d68bb555
Dan Fingal-Surma <dfingal@gmail.com>
2025-01-11 02:30:21 -0800

    Update problem_17.main.kts

--- src/2015/problem_17.main.kts
+++ src/2015/problem_17.main.kts
@@ -5,15 +5,15 @@ class Jug(val liters: Int)
 val lines = java.io.File(args[0]).readLines()
 val jugs = lines.map { Jug(it.toInt()) }
 
-fun fitCheck(used: List<Jug>, unused: List<Jug>, remaining: Int): List<List<Jug>> {
+fun fitCheck(used: List<Jug>, unused: List<Jug>, remaining: Int): List<List<Jug>> =
     if (remaining == 0) {
-        return listOf(used)
+        listOf(used)
+    } else {
+        val canUse = unused.filter { it.liters <= remaining }
+        canUse.withIndex().flatMap { (i, jug) -> 
+            fitCheck(used + jug, canUse.drop(i + 1), remaining - jug.liters) 
+        }
     }
-    val canUse = unused.filter { it.liters <= remaining }
-    return canUse.withIndex().flatMap { (i, jug) -> 
-        fitCheck(used + jug, canUse.drop(i + 1), remaining - jug.liters) 
-    }
-}
 
 val combos = fitCheck(emptyList(), jugs, 150)
 println(combos.size)
