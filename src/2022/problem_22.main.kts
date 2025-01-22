3be3fdca9e1d5ff7e6dda4cae687157138ca8110
Dan Fingal-Surma <dfingal@gmail.com>
2025-01-22 15:59:57 -0800

    Update problem_22.main.kts

--- src/2022/problem_22.main.kts
+++ src/2022/problem_22.main.kts
@@ -24,50 +24,50 @@ fun parseInstructions(input: String): List<Instruction> {
     return output
 }
 
-typealias Map = List<String>
 data class Position(val x: Int, val y: Int, val dir: Char)
-
-fun Map.next(p: Position): Position {
-    if (p.dir == '<' || p.dir == '>') {
-        val line = get(p.y)
-        val xMin = line.indexOfFirst { it != ' '}
-        val xMax = line.indexOfLast { it != ' '}
-        var newX = p.x + if (p.dir == '>') 1 else -1
-        if (newX > xMax) newX = xMin
-        if (newX < xMin) newX = xMax
-        return if (line[newX] == '#') p else p.copy(x = newX)
-    } else {
-        val yMin = indexOfFirst { it.getOrElse(p.x) { ' ' } != ' '}
-        val yMax = indexOfLast { it.getOrElse(p.x) { ' ' } != ' '}
-        var newY = p.y + if (p.dir == 'v') 1 else -1
-        if (newY > yMax) newY = yMin
-        if (newY < yMin) newY = yMax
-        return if (get(newY).getOrNull(p.x) == '#') p else p.copy(y = newY)
-    }
-}
-
-fun Map.walk(): Int {
-    var p = Position(get(0).indexOf('.'), 0, '>')
-    for (instruction in instructions) {
-        check(this[p.y][p.x] == '.')
-        when (instruction) {
-            is Face -> p = p.copy(dir = instruction.dir)
-            is Move -> {
-                repeat (instruction.steps) {
-                    p = next(p)
-                    check(this[p.y][p.x] == '.')
-                }
-            }
+data class Map(val lines: List<String>) {
+    fun Position.next(): Position {
+        if (dir == '<' || dir == '>') {
+            val line = lines[y]
+            val xMin = line.indexOfFirst { it != ' '}
+            val xMax = line.indexOfLast { it != ' '}
+            var newX = x + if (dir == '>') 1 else -1
+            if (newX > xMax) newX = xMin
+            if (newX < xMin) newX = xMax
+            return if (line[newX] == '#') this else copy(x = newX)
+        } else {
+            val yMin = lines.indexOfFirst { it.getOrElse(x) { ' ' } != ' '}
+            val yMax = lines.indexOfLast { it.getOrElse(x) { ' ' } != ' '}
+            var newY = y + if (dir == 'v') 1 else -1
+            if (newY > yMax) newY = yMin
+            if (newY < yMin) newY = yMax
+            return if (lines[newY][x] == '#') this else copy(y = newY)
         }
     }
 
-    val row = p.y + 1
-    val column = p.x + 1
-    val facing = ">v<^".indexOf(p.dir)
-    return 1000 * row + 4 * column + facing
+    fun walk(): Int {
+        var p = Position(lines[0].indexOf('.'), 0, '>')
+        for (instruction in instructions) {
+            check(lines[p.y][p.x] == '.')
+            when (instruction) {
+                is Face -> p = p.copy(dir = instruction.dir)
+                is Move -> {
+                    repeat (instruction.steps) {
+                        p = p.next()
+                        check(lines[p.y][p.x] == '.')
+                    }
+                }
+            }
+        }
+
+        val row = p.y + 1
+        val column = p.x + 1
+        val facing = ">v<^".indexOf(p.dir)
+        return 1000 * row + 4 * column + facing
+    }
 }
 
-val lines = java.io.File(args[0]).readLines()
-val map = lines.dropLast(2)
-val instructions = parseInstructions(lines.last())
+val input = java.io.File(args[0]).readLines()
+val map = Map(input.dropLast(2))
+val instructions = parseInstructions(input.last())
 println(map.walk())
