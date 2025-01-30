81e46b787e925ca8d9fae09f582c1ab87a3f6828
Dan Fingal-Surma <dfingal@gmail.com>
2025-01-30 13:05:39 -0800

    Update problem_17.main.kts

--- src/2023/problem_17.main.kts
+++ src/2023/problem_17.main.kts
@@ -1,5 +1,8 @@
 #!/usr/bin/env kotlin
 
+import kotlin.math.min
+import kotlin.math.max
+
 data class Point(val x: Int, val y: Int)
 enum class Direction(val dx: Int, val dy: Int) { NORTH(0, -1),  EAST(1, 0), SOUTH(0, 1), WEST(-1, 0) }
 operator fun Point.plus(d: Direction) = Point(x + d.dx, y + d.dy)
@@ -7,29 +10,45 @@ operator fun Point.plus(d: Direction) = Point(x + d.dx, y + d.dy)
 val turnRight = (Direction.entries + Direction.NORTH).zipWithNext().toMap()
 val turnLeft = (Direction.entries.reversed() + Direction.WEST).zipWithNext().toMap()
 
-data class State(val p: Point, val d: Direction, val i: Int)
+data class State(val p: Point, val d: Direction)
 
 fun State.next() = buildList {
-    if (i < 3) add(copy(p = p + d, i = i + 1))
     val l = turnLeft.getValue(d)
-    add(State(p + l, l, 1))
     val r = turnRight.getValue(d)
-    add(State(p + r, r, 1))
+    add(State(p + d, l))
+    add(State(p + d, r))
+    add(State(p + d + d, l))
+    add(State(p + d + d, r))
+    add(State(p + d + d + d, l))
+    add(State(p + d + d + d, r))
 }
 
 fun findShortestPath(costs: List<List<Int>>): Int {
-    val start = State(Point(0, 0), Direction.EAST, 0)
+    val start1 = State(Point(0, 0), Direction.EAST)
+    val start2 = State(Point(0, 0), Direction.SOUTH)
     val end = Point(costs[0].lastIndex, costs.lastIndex)
-    val frontier = mutableSetOf(start to 0)
+    val frontier = mutableSetOf(start1, start2)
+    val minCost = mutableMapOf(start1 to 0, start2 to 0)
     val seen = mutableSetOf<State>()
     while (!frontier.isEmpty()) {
-        val (current, cost) = frontier.minBy { it.second }
-        frontier.remove(current to cost)
+        val current = frontier.minBy { minCost.getValue(it) }
+        val cost = minCost.getValue(current)
+        frontier.remove(current)
         if (!seen.add(current)) continue
         if (current.p == end) return cost
         for (next in current.next()) {
             if (next.p.x in costs[0].indices && next.p.y in costs.indices) {
-                frontier.add(next to (cost + costs[next.p.y][next.p.x]))
+                val (x1, y1) = current.p
+                val (x2, y2) = next.p
+                val nextCost = cost + if (x1 == x2) {
+                    (min(y1 + 1, y2)..max(y1 - 1, y2)).sumOf { costs[it][x1] }
+                } else {
+                    (min(x1 + 1, x2)..max(x1 - 1, x2)).sumOf { costs[y1][it] }
+                }
+                if (nextCost < minCost[next] ?: Int.MAX_VALUE) {
+                    minCost[next] = nextCost
+                    frontier.add(next)
+                }
             }
         }
     }
