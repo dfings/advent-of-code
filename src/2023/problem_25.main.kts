957b566b8bde706286f984b4c717c59a91ed8ead
Dan Fingal-Surma <dfingal@gmail.com>
2025-01-29 18:24:16 -0800

    Update problem_25.main.kts

--- src/2023/problem_25.main.kts
+++ src/2023/problem_25.main.kts
@@ -1,12 +1,29 @@
 #!/usr/bin/env kotlin
 
-fun parse(lines: List<String>): Map<String, List<String>> {
-    val graph = mutableMapOf<String, MutableList<String>>()
+class Graph {
+    private val map = mutableMapOf<String, MutableList<String>>()
+
+    fun nodes() = map.keys
+    fun edges(a: String) = map[a] ?: emptyList()
+
+    fun addEdge(a: String, b: String) {
+        map.getOrPut(a) { mutableListOf<String>() }.add(b)
+        map.getOrPut(b) { mutableListOf<String>() }.add(a)   
+    }
+
+    fun removeNode(a: String) {
+        for (c in map.remove(a) ?: emptyList()) {
+            map.getValue(c).removeAll { it == a }
+        }
+    }
+}
+
+fun parse(lines: List<String>): Graph {
+    val graph = Graph()
     for (line in lines) {
         val (a, bs) = line.split(": ").let { it[0] to it[1].split(" ")}
         for (b in bs) {
-            graph.getOrPut(a) { mutableListOf<String>() }.add(b)
-            graph.getOrPut(b) { mutableListOf<String>() }.add(a)
+            graph.addEdge(a, b)
         }
     }
     return graph
@@ -14,22 +31,21 @@ fun parse(lines: List<String>): Map<String, List<String>> {
 
 fun solve(lines: List<String>) {
     while (true) {
-        val graph = parse(lines).toMutableMap()
-        val counts = graph.keys.associateWith { 1 }.toMutableMap()
-        while (graph.keys.size > 2) {
-            val a = graph.keys.random()
-            val b = graph.getValue(a).random()
+        val graph = parse(lines)
+        val counts = graph.nodes().associateWith { 1 }.toMutableMap()
+        while (graph.nodes().size > 2) {
+            val a = graph.nodes().random()
+            val b = graph.edges(a).random()
             val ab = "$a-$b"
             counts[ab] = (counts.remove(a) ?: 0) + (counts.remove(b) ?: 0)
-            graph[ab] = graph.getValue(a).filter { it != b } + graph.getValue(b).filter { it != a }
-            graph.remove(a)?.forEach { c ->
-                graph[c] = graph.getValue(c).map { if (it == a) ab else it }
-            }
-            graph.remove(b)?.forEach { c ->
-                graph[c] = graph.getValue(c).map { if (it == b) ab else it }
+            val connected = graph.edges(a) + graph.edges(b)
+            graph.removeNode(a)
+            graph.removeNode(b)
+            for (c in connected.filter { it != a && it != b }) {
+                graph.addEdge(ab, c)
             }
         }
-        if (graph.entries.first().value.size == 3) {
+        if (graph.edges(graph.nodes().first()).size == 3) {
             println(counts.values.reduce(Int::times))
             break
         }
