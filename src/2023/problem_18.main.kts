893e7911a46c108f019decce8a3f91f8cf9ebba0
Dan Fingal-Surma <dfingal@gmail.com>
2025-01-31 01:48:38 -0800

    Update problem_18.main.kts

--- src/2023/problem_18.main.kts
+++ src/2023/problem_18.main.kts
@@ -1,39 +1,48 @@
 #!/usr/bin/env kotlin
 
-data class Point(val x: Int, val y: Int)
-enum class Direction(val dx: Int, val dy: Int) { NORTH(0, -1),  EAST(1, 0), SOUTH(0, 1), WEST(-1, 0) }
-operator fun Point.plus(d: Direction) = Point(x + d.dx, y + d.dy)
+import kotlin.math.abs
 
-data class Instruction(val direction: Direction, val distance: Int, val color: String)
+data class Point(val x: Long, val y: Long)
+enum class Direction(val dx: Long, val dy: Long) { EAST(1, 0), SOUTH(0, 1), WEST(-1, 0), NORTH(0, -1) }
+fun Point.move(d: Direction, distance: Long) = Point(x + d.dx * distance, y + d.dy * distance)
+
+data class Instruction(val direction: Direction, val distance: Long)
 
 val pattern = Regex("""(\w) (\d+) \(#(\w+)\)""")
 val dirs = mapOf("U" to Direction.NORTH, "R" to Direction.EAST,
                  "D" to Direction.SOUTH, "L" to Direction.WEST)
-fun parse(input: String): Instruction {
+fun parse(input: String): Pair<Instruction, Instruction> {
     val (a, b, c) = pattern.find(input)!!.destructured
-    return Instruction(dirs.getValue(a), b.toInt(), c)
+    return Instruction(dirs.getValue(a), b.toLong()) to 
+           Instruction(Direction.entries[c.takeLast(1).toInt()], c.dropLast(1).toLong(16))
 }
+
+val outer =(Direction.entries + Direction.EAST).zipWithNext().toMap()
+
+fun List<Instruction>.getPoints() : List<Point> {
+    val output = mutableListOf<Point>()
+    var current = Point(0, 0)
+    output.add(current)
+    var lastOuter = true
+    for (i in indices) {
+        val (dir, len) = get(i)
+        val turn = get((i + 1) % size).direction
+        current = current.move(dir, len)
+        val thisOuter = outer[dir] == turn
+        if (lastOuter && thisOuter) current = current.move(dir, 1)
+        if (!lastOuter && !thisOuter) current = current.move(dir, -1)
+        lastOuter = thisOuter
+        output.add(current)
+    }
+    return output
+}
+
+fun List<Point>.area() = zipWithNext().sumOf { (a, b) -> a.x * b.y - b.x * a.y } / 2L
 
 fun solve(lines: List<String>) {
     val instructions = lines.map { parse(it) }
-    var current = Point(0, 0)
-    val filled = mutableSetOf(current)
-    for (instruction in instructions) {
-        repeat(instruction.distance) {
-            current = current + instruction.direction
-            filled.add(current)
-        }
-    }
-    val minPoint = filled.minBy { it.x + it.y }
-    check(Point(minPoint.x + 1, minPoint.y + 1) !in filled)
-    val frontier = ArrayDeque(listOf(Point(minPoint.x + 1, minPoint.y + 1)))
-    while (!frontier.isEmpty()) {
-        val current = frontier.removeFirst()
-        if (!filled.add(current)) continue
-        frontier.addAll(Direction.entries.map { current + it }.filter { it != filled })
-    }
-
-    println(filled.size)
+    println(instructions.map { it.first }.getPoints().area())
+    println(instructions.map { it.second }.getPoints().area())
 }
 
 solve(java.io.File(args[0]).readLines())
