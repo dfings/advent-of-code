0877e11d5f54feb597e8bdfc52f9cf22743d5289
Dan Fingal-Surma <dfingal@gmail.com>
2025-12-01 00:35:46 -0800

    Working solution, needs clean up

--- src/2025/problem_1.main.kts
+++ src/2025/problem_1.main.kts
@@ -1,8 +1,18 @@
 #!/usr/bin/env kotlin
 
+import kotlin.math.abs
+
 fun solve(lines: List<String>) {
     val turns = lines.map { (if (it[0] == 'L') -1  else 1) * it.drop(1).toInt() }
-    println(turns.runningFold(50) { acc, it -> (acc + it).mod(100) }.count { it == 0 })
+    val positions = turns.runningFold(50) { acc, it -> (acc + it).mod(100) }
+    println(positions.count { it == 0 })
+
+    val fullTurns = turns.sumOf { abs(it) / 100 }
+    val partialTurns = positions.zipWithNext().zip(turns).count { (pair, turn) ->
+        val (a, b) = pair
+        a != 0 && (b == 0 || ((turn < 0 && b > a) || (turn > 0 && b < a)))
+    }
+    println(fullTurns + partialTurns)
 }
 
 solve(java.io.File(args[0]).readLines())
