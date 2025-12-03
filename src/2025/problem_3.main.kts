28be2dfd0dbd87a4fa6aa88790588971a4de62ef
Dan Fingal-Surma <dfingal@gmail.com>
2025-12-02 21:12:41 -0800

    Update problem_3.main.kts

--- src/2025/problem_3.main.kts
+++ src/2025/problem_3.main.kts
@@ -1,14 +1,18 @@
 #!/usr/bin/env kotlin
 
-fun maxJoltage(bank: String): Int {
-    val first = bank.dropLast(1).maxBy { "$it".toInt() }
-    val firstIndex = bank.indexOf(first)
-    val second = bank.drop(firstIndex + 1).maxBy { "$it".toInt() }
-    return "$first$second".toInt()
+fun maxJoltage(bank: String, n: Int): Long {
+    val best = bank.dropLast(n - 1).maxBy { "$it".toInt() }
+    return if (n == 1) {
+        "$best".toLong()
+    } else {
+        val rest = maxJoltage(bank.drop(bank.indexOf(best) + 1), n - 1)
+        "$best$rest".toLong()
+    }
 }
 
 fun solve(input: List<String>) {
-    println(input.sumOf { maxJoltage(it) })
+    println(input.sumOf { maxJoltage(it, 2) })
+    println(input.sumOf { maxJoltage(it, 12) })
 }
 
 solve(java.io.File(args[0]).readLines())
