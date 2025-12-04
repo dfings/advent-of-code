2f56a2724f0a705fe3282af615106cde98049ae2
Dan Fingal-Surma <dfingal@gmail.com>
2025-12-03 21:26:37 -0800

    Update problem_4.main.kts

--- src/2025/problem_4.main.kts
+++ src/2025/problem_4.main.kts
@@ -1,25 +1,16 @@
 #!/usr/bin/env kotlin
 
-fun List<List<Char>>.isPaper(x: Int, y: Int): Boolean {
-    if (y !in indices || x !in get(y).indices) return false
-    return get(y).get(x) == '@'
-}
+fun List<List<Char>>.isPaper(x: Int, y: Int): Boolean =
+    y in indices && x in get(y).indices && this[y][x] == '@'
 
-fun List<List<Char>>.countAdjacent(x: Int, y: Int): Int {
-    var count = 0
-    for (j in -1..1) {
-        for (i in -1..1) {
-            if ((j != 0 || i != 0 )&& isPaper(x + i, y + j)) count++
-        }
-    }
-    return count
-}
+fun List<List<Char>>.countPaper(x: Int, y: Int): Int =
+    (-1..1).sumOf{ j -> (-1..1).sumOf { i -> if (isPaper(x + i, y + j)) 1 else 0 } }
 
-fun MutableList<MutableList<Char>>.canRemove(remove: Boolean): Int {
+fun MutableList<MutableList<Char>>.maybeRemove(remove: Boolean): Int {
     var count = 0
     for (y in indices) {
         for (x in get(y).indices) {
-            if (this[y][x] == '@' && countAdjacent(x, y) < 4) {
+            if (this[y][x] == '@' && countPaper(x, y) < 5) {
                 if (remove) this[y][x] = '.'
                 count++
             }
@@ -30,12 +21,10 @@ fun MutableList<MutableList<Char>>.canRemove(remove: Boolean): Int {
 
 fun solve(input: List<String>) {
     val graph = input.map { it.toMutableList() }.toMutableList()
-    println(graph.canRemove(false))
+    println(graph.maybeRemove(false))
     var count = 0
     while (true) {
-        val x = graph.canRemove(true)
-        if (x == 0) break
-        count += x
+        count += graph.maybeRemove(true).also { if (it == 0) break }
     }
     println(count)
 }
