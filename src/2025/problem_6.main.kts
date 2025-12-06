abe842c4882a90869dd5d63b97c76a3066c9f92a
Dan Fingal-Surma <dfingal@gmail.com>
2025-12-05 22:23:18 -0800

    Update problem_6.main.kts

--- src/2025/problem_6.main.kts
+++ src/2025/problem_6.main.kts
@@ -2,13 +2,13 @@
 
 val whitespace = "\\s+".toRegex()
 
-fun parse1(input: List<String>): List<List<Long>> {
-    val numbers = input.map { it.trim().split(whitespace).map { it.toLong() } }
-    return numbers[0].indices.map { i -> numbers.map { it[i] } }
-}
+fun <T> List<List<T>>.transpose() = first().indices.map { i -> map { it[i] } }
+
+fun parse1(input: List<String>): List<List<Long>> =
+    input.map { it.trim().split(whitespace).map { it.toLong() } }.transpose()
 
 fun parse2(input: List<String>): List<List<Long>> {
-    val transpose = input[0].indices.map { i -> input.map { it[i] }.joinToString("").trim() }
+    val transpose = input.map { it.toList() }.transpose().map { it.joinToString("").trim() }
     val numbers = mutableListOf(mutableListOf<Long>())
     for (line in transpose) {
         if (line.isEmpty()) {
