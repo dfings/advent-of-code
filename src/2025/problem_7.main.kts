016d18a109502b8d22730bf9442949b7ba14b7a6
Dan Fingal-Surma <dfingal@gmail.com>
2025-12-07 01:12:31 -0800

    Create problem_7.main.kts

---
+++ src/2025/problem_7.main.kts
@@ -0,0 +1,45 @@
+#!/usr/bin/env kotlin
+
+data class Point(val x: Int, val y: Int)
+
+class GameState(val start: Point, val splitters: Set<Point>) {
+    val yMax = splitters.maxOf { it.y } + 1
+    var current = setOf<Point>(start)
+    val paths = mutableMapOf(start to 1L)
+
+    fun step(): Boolean {
+        if (current.first().y == yMax) return false
+        val next = mutableListOf<Point>()
+        for (p in current) {
+            val n = p.copy(y = p.y + 1)
+            link(n, p)
+            if (n in splitters) {
+                for (s in listOf(n.copy(x = n.x - 1), n.copy(x = n.x + 1))) {
+                    next.add(s)
+                    link(s, p)
+                }
+            } else {
+                next.add(n)
+            }
+        }
+        current = next.toSet()
+        return true
+    }
+
+    fun link(next: Point, prev: Point) {
+        paths[next] = paths.getOrDefault(next, 0L) + paths.getValue(prev)
+    }
+}
+
+fun solve(input: List<String>) {
+    val start = Point(input[0].indexOf('S'), 0)
+    val splitters = input.flatMapIndexed { y, line -> 
+        line.mapIndexedNotNull { x, it -> if (it == '^') Point(x, y) else null } 
+    }.toSet()
+    val gameState = GameState(start, splitters)
+    while (gameState.step()) {}
+    println(gameState.splitters.count { it in gameState.paths.keys })
+    println(gameState.paths.entries.filter { it.key.y == gameState.yMax }.sumOf { it.value })
+}
+
+solve(java.io.File(args[0]).readLines())
