4772c886dd1aa08b00b2f57ec1a1ed6ecc0c0433
Dan Fingal-Surma <dfingal@gmail.com>
2025-12-11 02:28:36 -0800

    Update problem_10.main.kts

--- src/2025/problem_10.main.kts
+++ src/2025/problem_10.main.kts
@@ -39,55 +39,71 @@ fun Machine1.solve(): Int {
     throw IllegalStateException("Path not found")
 }
 
-data class Machine2(val target: List<Int>, val buttons: List<List<Int>>)
-data class State2(val value: List<Int>, val cost: Int, val lastButtonIndex: Int)
-
+data class Machine2(val target: List<Int>, val buttons: List<List<Int>>, val targetOrder: List<Int>)
 fun String.parse2(): Machine2 {
     val target = substring(indexOf("{") + 1, lastIndex).split(",").map { it.toInt() }
     val buttons = substring(indexOf("("), lastIndexOf(")") + 1).split(" ").map {
         it.substring(1, it.lastIndex).split(",").map { it.toInt() }
-    }.sortedBy { -it.sum() }
-
-    return Machine2(target, buttons)
-}
-
-var solved = 0
-fun Machine2.solve(): Int {
-    val initialValue = List(target.size) { 0 }
-    val minCost = mutableMapOf(initialValue to 0)
-    val frontier = java.util.PriorityQueue<State2> { a, b -> a.cost.compareTo(b.cost) }
-    frontier.add(State2(initialValue, 0, 0))
-    while (!frontier.isEmpty()) {
-        val state = frontier.minBy { it.cost }
-        frontier.remove(state)
-        if (state.value == target) { 
-            return state.cost
-        }
-        for (index in (state.lastButtonIndex..buttons.lastIndex)) {
-            val button = buttons[index]
-            val newValue = state.value.toMutableList()
-            for (light in button) {
-                newValue[light]++
-            }
-            if (newValue.indices.any { i -> newValue[i] > target[i] }) continue
-            val oldCost = minCost[newValue] ?: Int.MAX_VALUE
-            val newCost = state.cost + 1
-            if (newCost < oldCost) {
-                minCost[newValue] = newCost
-                frontier.add(State2(newValue, newCost, index))
-            }
-        }
     }
-    throw IllegalStateException("Path not found")
+
+    var buttonCount = target.indices.map { i -> buttons.count { i in it } }
+    val targetOrder = buttonCount.withIndex().sortedWith(compareBy({ it.value}, {-target[it.index] })).map { it.index }
+    return Machine2(target, buttons, targetOrder)
 }
 
+val cache = mutableMapOf<List<Int>, Int>()
+fun Machine2.solve(): Int {
+    cache.clear()
+    return solve(
+        target.map { 0 }, 
+        0, 
+        targetOrder[0], 
+        buttons.filter { targetOrder[0] in it }.sortedBy { -it.size }, 
+        buttons.filter { targetOrder[0] !in it },
+    )
+}
+
+fun Machine2.solve(
+    current: List<Int>, 
+    cost: Int, 
+    currentTarget: Int, 
+    currentButtons: List<List<Int>>, 
+    remainingButtons: List<List<Int>>
+): Int = cache.getOrPut(current) {
+    currentButtons.indices.minOfOrNull { index ->
+        val button = currentButtons[index]
+        val newValue = current.toMutableList()
+        for (light in button) {
+            newValue[light]++
+        }
+        val newButtons = if (index == 0) currentButtons else currentButtons.drop(index)
+        when {
+            newValue == target -> cost + 1
+            newValue.indices.any { i -> newValue[i] > target[i] } -> 100_000
+            newValue[currentTarget] == target[currentTarget] -> {
+                val newTarget = targetOrder[targetOrder.indexOf(currentTarget) + 1]
+                solve(
+                    newValue,
+                    cost + 1,
+                    newTarget,
+                    remainingButtons.filter { newTarget in it }.sortedBy { -it.size },
+                    remainingButtons.filter { newTarget !in it },
+                )
+            }
+            else -> solve(newValue, cost + 1, currentTarget, newButtons, remainingButtons)
+        }
+    } ?: 100_000
+}
 
 fun solve(lines: List<String>) {
     val machines1 = lines.map { it.parse1() }
     println(machines1.sumOf { it.solve() })
 
     val machines2 = lines.map { it.parse2() }
-    println(machines2.sumOf { it.solve() })
+    println(machines2.sumOf { 
+        println(it)
+        it.solve() 
+    })
 }
 
 solve(java.io.File(args[0]).readLines())
