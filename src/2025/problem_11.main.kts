8910a4fc81e1197370da9cb5e8c20ce245ce2c92
Dan Fingal-Surma <dfingal@gmail.com>
2025-12-10 22:27:12 -0800

    Update problem_11.main.kts

--- src/2025/problem_11.main.kts
+++ src/2025/problem_11.main.kts
@@ -7,14 +7,25 @@ fun parse(lines: List<String>): Map<String, List<String>> = buildMap {
     }
 }
 
-val pathCountCache = mutableMapOf<String, Int>()
-fun Map<String, List<String>>.countPaths(from: String): Int = pathCountCache.getOrPut(from) {
-    if (from == "out") 1 else getValue(from).sumOf { countPaths(it) }
+data class PathCount(val total: Long, val dac: Long, val fft: Long, val both: Long)
+
+val pathCountCache = mutableMapOf<String, PathCount>()
+fun Map<String, List<String>>.countPaths(from: String): PathCount = pathCountCache.getOrPut(from) {
+    if (from == "out") return@getOrPut PathCount(1, 0, 0, 0)
+    val out = getValue(from).map { countPaths(it) }.reduce { a, b -> 
+        PathCount(a.total + b.total, a.dac + b.dac, a.fft + b.fft, a.both + b.both)
+    }
+    when (from) {
+        "dac" -> out.copy(dac = out.total, both = if (out.fft > 0) out.fft else 0)
+        "fft" -> out.copy(fft = out.total, both = if (out.dac > 0) out.dac else 0)
+        else -> out
+    }
 }
 
 fun solve(lines: List<String>) {
     val deviceMap = parse(lines)
-    println(deviceMap.countPaths("you"))
+    println(deviceMap.countPaths("you").total)
+    println(deviceMap.countPaths("svr").both)
 }
 
 solve(java.io.File(args[0]).readLines())
