a71b93132ef339a09c08ca0bf18d52c1dc3d4e70
Dan Fingal-Surma <dfingal@gmail.com>
2025-12-11 22:56:09 -0800

    Update problem_12.main.kts

--- src/2025/problem_12.main.kts
+++ src/2025/problem_12.main.kts
@@ -11,9 +11,14 @@ fun String.parseArea(): Area {
 fun solve(lines: List<String>) {
     val shapes = lines.takeWhile { "x" !in it }.chunked(5).map { it.joinToString().count { it == '#' } }
     val areas = lines.takeLastWhile { it.isNotEmpty() }.map { it.parseArea() }
-    println(areas.count { area ->
+    val fullSquareCount = areas.count { area ->
+        (area.length / 3 * 3) * (area.width / 3 * 3) >= area.boxes.map { it * 9 }.sum()
+    }
+    val perfectPackCount = areas.count { area ->
         area.length * area.width >= area.boxes.mapIndexed { i, it -> it * shapes[i] }.sum()
-    })
+    }
+    check(fullSquareCount == perfectPackCount)
+    println(fullSquareCount)
 }
 
 solve(java.io.File(args[0]).readLines())
